1ecb480bae72a019070fed215669d3062bcccb8a
Huangxin <maggiewangsjtu@gmail.com>
2014-05-30 01:12:04 -0400

    fix bug
    insrtio-sort-list

--- Insertion-Sort-List.java
+++ Insertion-Sort-List.java
@@ -20,24 +20,29 @@ public class Solution {
         
         if(head==null || head.next==null) return head;
         
+        //create a preHead that points to head in case there are only two nodes
+        //e.g. the list is {3,2}, using insertion sort, then a preHead is needed 
         ListNode preHead = new ListNode(0);
         preHead.next = head;
         
         ListNode curr = head;
         
         
-        while(curr.next!=null && curr!=null)
+        while(curr!=null)
         {
             while(curr.next!=null  && curr.next.val < curr.val)
             {
-                ListNode tmp = head;
+                ListNode tmp = preHead.next;
                 ListNode preTmp = preHead;
+                
+                //find the position to insert the new element (i.e., curr.next)
                 while(tmp.val < curr.next.val)
                 {
                     preTmp = tmp;
                     tmp = tmp.next;
                 }
                 
+                //insert the new element after preTmp and before tmp pointer
                 preTmp.next = curr.next;
                 curr.next = curr.next.next;
                 preTmp.next.next = tmp;
