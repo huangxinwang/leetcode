aab098f9263b2362e15060cb0ee8f3675b03be28
Huangxin <maggiewangsjtu@gmail.com>
2014-06-02 01:26:47 -0400

    initial implementation
    implemente binary tree inorder traversal

---
+++ Binary-Tree-Inorder-Traversal.java
@@ -0,0 +1,35 @@
+/**
+ * Problem Statement
+ * Given a binary tree, return the inorder traversal of its nodes' values.
+ */
+ 
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    
+    ArrayList<Integer> rnt = new ArrayList<Integer>();
+    
+    public ArrayList<Integer> inorderTraversal(TreeNode root) {
+        
+        if(root==null)
+            return rnt;
+            
+        if(root.left!=null) 
+            inorderTraversal(root.left);
+            
+        rnt.add(root.val);
+        
+        if(root.right!=null)
+            inorderTraversal(root.right);
+        
+        return rnt;
+    }
+    
+}
