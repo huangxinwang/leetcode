e6a7aee23b1b62c4928494c57e16734c4cdeecaa
Huangxin <maggiewangsjtu@gmail.com>
2014-06-10 22:44:22 -0400

    init

--- Evaluate-Reverse-Polish-Notation.java
+++ Evaluate-Reverse-Polish-Notation.java
@@ -6,33 +6,38 @@
  * 
  */
 
+import java.util.regex.*;
 
 public class Solution {
     
     public int evalRPN(String[] tokens) {
         
-        Patten p = new Patten("[\\d]+");
+        Pattern  p = Pattern.compile("[\\d]+");
         ArrayList<Integer> list = new ArrayList<Integer>();
         
-        for(int i=0; i<tokens; i++){
+        for(int i=0; i<tokens.length; i++){
+			
+			//judge if it is an integer or symbol
             Matcher m = p.matcher(tokens[i]);
             if(m.matches()){
-                list.add(Integer.parse(tokens[i]));
+                list.add(Integer.parseInt(tokens[i]));
             }else{
-                int num1 = list.remove(list.size()-1);
+				//if it is symbol, pop out numbers for calculation
+				//then push back the results
                 int num2 = list.remove(list.size()-1);
+                int num1 = list.remove(list.size()-1);
                 int num3 = 0;
                 switch(tokens[i]){
-                    case '+':
+                    case "+":
                         num3 = num1 + num2;
                         break;
-                    case '-':
+                    case "-":
                         num3 = num1 - num2;
                         break;
-                    case '/';
+                    case "/":
                         num3 = num1/num2;
                         break;
-                    case '*':
+                    case "*":
                         num3 = num1*num2;
                         break;
                 }
@@ -40,7 +45,7 @@ public class Solution {
             }
         }
         
-     return list(0);
+     return list.get(0);
      
     }
 }
