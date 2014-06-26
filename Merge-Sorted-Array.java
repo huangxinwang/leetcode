209f3a043cd7a29617729496ef5ec32baf34a9bc
Huangxin <maggiewangsjtu@gmail.com>
2014-06-25 21:24:46 -0400

    new implementation

---
+++ Merge-Sorted-Array.java
@@ -0,0 +1,35 @@
+/**
+ * Problem Statement: Merge Sorted Array 
+ * 
+ * Given two sorted integer arrays A and B, merge B into A as one sorted array.
+ * 
+ * Note:
+ * You may assume that A has enough space (size that is greater or equal to m + n) to hold additional elements from B. 
+ * The number of elements initialized in A and B are m and n respectively.
+ */
+
+
+public class Solution {
+    public void merge(int A[], int m, int B[], int n) {
+        
+        //put elements to A
+        //note we start from the end in order not to overwrite elements in A
+        while(m>=1 && n>=1){
+            
+            if(A[m-1] >= B[n-1]){
+                A[m+n-1] = A[m-1];
+                m--;
+            }
+            else{
+                A[m+n-1] = B[n-1];
+                n--;
+            }
+        }
+        
+        //if B hasn't traversed to the end, continue moving elements to A
+        while(n>=1){
+            A[m+n-1] = B[n-1];
+            n--;
+        }
+    }
+}
