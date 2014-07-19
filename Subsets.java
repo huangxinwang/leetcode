04e80a402c40aba2cee6644bb824b44cc31f153b
Huangxin Wang <maggiewangsjtu@gmail.com>
2014-07-18 20:32:06 -0400

    update notes

---
+++ Subsets.java
@@ -0,0 +1,44 @@
+/**
+ * Problem Statement: Subsets
+ * Given a set of distinct integers, S, return all possible subsets.
+ * 
+ * Link: https://oj.leetcode.com/problems/subsets/
+ * 
+ * Elements in a subset must be in non-descending order.
+ * The solution set must not contain duplicate subsets.
+ */
+
+
+public class Solution {
+    public ArrayList<ArrayList<Integer>> subsets(int[] S) {
+        //return parameter
+        ArrayList<ArrayList<Integer>> rnt = new ArrayList<ArrayList<Integer>>();
+        if(S.length==0) return rnt;
+        ArrayList<Integer> curr = new ArrayList<Integer>();
+        rnt.add(new ArrayList<Integer>());
+        
+        //change the size of subsets
+        for(int i=1; i<=S.length; i++)
+            dfs(S,0,0,i,rnt,curr);
+        return rnt;
+        
+    }
+    
+    public void dfs(int[] S, int start, int depth, int size, ArrayList<ArrayList<Integer>> rnt, ArrayList<Integer> curr){
+        //finish condition
+        if(depth==size){
+            //sort list
+            ArrayList<Integer> finish = new ArrayList<Integer>(curr);
+            Collections.sort(finish);
+            rnt.add(finish);
+            return;
+        }
+        
+        //dfs
+        for(int i=start; i<S.length; i++){
+            ArrayList<Integer> tmp = new ArrayList<Integer>(curr);
+            tmp.add(S[i]);
+            dfs(S, i+1, depth+1, size, rnt, tmp);
+        }
+    }
+}
