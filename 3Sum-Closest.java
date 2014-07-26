cae066164e691eca0db952f47d5373f2db796d02
Huangxin <maggiewangsjtu@gmail.com>
2014-07-26 19:05:25 -0400

    new implementation

---
+++ 3Sum-Closest.java
@@ -0,0 +1,45 @@
+/**
+ * Problem Statement: 3Sum Closest 
+ * link: https://oj.leetcode.com/problems/3sum-closest/
+ * Time: 840ms
+ * 
+ * Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target. 
+ * Return the sum of the three integers. You may assume that each input would have exactly one solution.
+ * For example, given array S = {-1 2 1 -4}, and target = 1.
+ * The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+ */
+
+
+public class Solution {
+    
+    public int threeSumClosest(int[] num, int target) {
+        
+        //keep two sets, oneSet for one element, twoSet for two elements
+        Set<Integer> setOne = new HashSet<Integer>();
+        Set<Integer> setTwo = new HashSet<Integer>();
+        
+        //keep optimal dist and solution
+        int minDist = Integer.MAX_VALUE;
+        int optValue = 0;
+        
+        //traverse tge arrays, and update the optimal dist and value
+        for(int i=0; i<num.length; i++){
+            
+            for(Integer ele : setTwo){
+                int sum = ele + num[i];
+                int dist = Math.abs(sum-target);
+                if(minDist>dist){
+                    minDist = dist;
+                    optValue = sum;
+                }
+            }
+            
+            for(Integer ele : setOne)
+                setTwo.add(ele+num[i]);
+                
+            setOne.add(num[i]);
+        }
+        
+        return optValue;
+    }
+}
