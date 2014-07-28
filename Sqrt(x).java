c9333631486f56c26ca753a1cf3805348d34d4a4
Huangxin Wang <maggiewangsjtu@gmail.com>
2014-07-28 17:07:45 -0400

    new implementation

---
+++ Sqrt(x).java
@@ -0,0 +1,30 @@
+/**
+ * Problem Statement: Sqrt(x) 
+ * Link:https://oj.leetcode.com/problems/sqrtx/
+ * Time: 476 ms
+ * 
+ * Implement int sqrt(int x).
+ * Compute and return the square root of x.
+ */
+
+
+public class Solution {
+    
+    public int sqrt(int x) {
+        int low = 0;
+        int high = x;
+        return (int)binarySearch(low, high, x);
+    }
+    
+    //use binary search to find root
+    public long binarySearch(long low, long high, int x){
+        if(low>=high) return low;
+        long mid = (low+high)/2;
+        if(mid*mid==x) return mid;
+        //this condition is necessary for successful implementing binary search
+        if(mid*mid<x && (mid+1)*(mid+1)>x) return mid;
+        //binar search
+        if(mid*mid<x) return binarySearch(mid+1, high, x);
+        else return binarySearch(low, mid-1, x);
+    }
+}
