36f27a6be7d6c0d493e4119f13e9156506fe7b17
Huangxin Wang <maggiewangsjtu@gmail.com>
2014-08-04 21:01:42 -0400

    new implementation

---
+++ Word-Search.java
@@ -0,0 +1,82 @@
+/**
+ * Problem Statement: Word Search 
+ * Link: https://oj.leetcode.com/problems/word-search/
+ * Time: 512ms
+ * 
+ * Given a 2D board and a word, find if the word exists in the grid.
+ * The word can be constructed from letters of sequentially adjacent cell, 
+ * where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+ * 
+ */
+
+public class Solution {
+    
+    int[] dx;
+    int[] dy;
+    int[][] state;
+    int row;
+    int col;
+    boolean found;
+    
+    public boolean exist(char[][] board, String word) {
+        
+        //intial variable
+        row = board.length;
+        col = board[0].length;
+        int len = word.length();
+        
+        //adjacent matrix
+        dx = new int[]{0,0,1,-1};
+        dy = new int[]{1,-1,0,0};
+        
+        //state, used by dfs to keep record of visited point
+        state = new int[row][col];
+        
+        found = false;
+        
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                //for each entry point, dfs find whether the word coud be constructured
+                if (board[i][j]==word.charAt(0))
+                    dfs(i,j,0,word,board);
+                if (found) return true;
+            }
+        }
+        
+        return found;
+    }
+    
+    
+    //two layers of dfs to find 
+    public void dfs(int x, int y, int depth, String word, char[][] board)
+    {
+        //terminate condition
+        if(found) return;
+        if(depth==word.length()-1) {found=true; return;}
+        
+        //visiting, change state
+        state[x][y] = 1;
+        
+        //visit his 4 adjacents provided the conditions satisified   
+        for(int i=0;i<4;i++)
+        {
+            int tx = x + dx[i];
+            int ty = y + dy[i];
+            
+            //check whether condition satisfied
+            if (canMove(tx, ty) && board[tx][ty]==word.charAt(depth+1))
+            {
+                dfs(tx, ty, depth+1, word, board);
+            }
+        }
+        
+        //finish visiting, change state
+        state[x][y] = 0;
+    }
+    
+    //check whether exceed border 
+    public boolean canMove(int tx, int ty){
+        if(tx<0 || tx>=row || ty<0 || ty>=col) return false;
+        return state[tx][ty]==0;
+    }
+}
