9f09c258e99475e78460f4f878d13e206e102e71
Huangxin <maggiewangsjtu@gmail.com>
2014-08-10 11:13:42 -0400

    new implementation

---
+++ Unique-Binary-Search-Trees-II.java
@@ -0,0 +1,56 @@
+/**
+ * Problem: Unique Binary Search Trees II 
+ * Link:https://oj.leetcode.com/problems/unique-binary-search-trees-ii/
+ * Time:480ms
+ * 
+ * Given n, generate all structurally unique BST's (binary search trees) that store values 1...n.
+ * For example,
+ * Given n = 3, your program should return all 5 unique BST's shown below.
+ *  1         3     3      2      1
+ *   \       /     /      / \      \
+ *   3     2     1      1   3      2
+ *  /     /       \                 \
+ * 2     1         2                 3
+ */
+
+
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; left = null; right = null; }
+ * }
+ */
+public class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        return dfs(1,n);
+    }
+    
+    //dfs find solutions
+    public ArrayList<TreeNode> dfs(int left, int right){
+        
+        ArrayList<TreeNode> rnt = new ArrayList<TreeNode>();
+        //terminate condition
+        if(left>right){
+            rnt.add(null);
+            return rnt;
+        }
+        
+        //build left, right tree
+        for(int i=left; i<=right; i++){
+            ArrayList<TreeNode> lefts = dfs(left,i-1);
+            ArrayList<TreeNode> rights = dfs(i+1,right);
+            for(TreeNode l: lefts){
+                for(TreeNode r: rights){
+                    TreeNode tmp = new TreeNode(i);
+                    tmp.left = l;
+                    tmp.right = r;
+                    rnt.add(tmp);
+                }
+            }
+        }
+        return rnt;
+    }
+}
