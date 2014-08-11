12f1319fc66221a5c38ea4187485c6d1ce5ef9f8
Huangxin Wang <maggiewangsjtu@gmail.com>
2014-08-11 17:21:11 -0400

    new implementation

--- Plus-One.java
+++ Plus-One.java
@@ -1,33 +1,37 @@
 /**
- * Problem Statement: Plus One
+ * Problem Statement: Plus One 
+ * Link:https://oj.leetcode.com/problems/plus-one/
+ * Time:360ms
+ * 
  * Given a non-negative number represented as an array of digits, plus one to the number.
  * The digits are stored such that the most significant digit is at the head of the list.
+ * 
  */
- 
- public class Solution {
+
+public class Solution {
     public int[] plusOne(int[] digits) {
-        int carry = 0;
-        if(digits.length==0) return digits;
-        
-        int[] remain = new int[digits.length];
+        int carry = 1;
+        //do calculation
         for(int i=digits.length-1; i>=0; i--){
-            if(i==digits.length-1) carry+=1;
             int sum = digits[i] + carry;
+            digits[i] = sum%10;
             carry = sum/10;
-            remain[i] = sum%10;
         }
         
-        
-        
-        if(carry==0) return remain;
+        //decide whether to expand array
+        if(carry==0) return digits;
         else{
             int[] rnt = new int[digits.length+1];
-            digits[0] = carry;
-            for(int i=1; i<digits.length+1; i++){
-                rnt[i] = remain[i-1];
-            }
+            arrayCopy(rnt,digits);
+            rnt[0] = 1;
             return rnt;
         }
-        
+    }
+    
+    //copy oldArr to newArr, from highest to lowest
+    public void arrayCopy(int[] newArr, int[] oldArr){
+        for(int i=oldArr.length; i>=1; i--){
+            newArr[i] = oldArr[i-1];
+        }
     }
 }
