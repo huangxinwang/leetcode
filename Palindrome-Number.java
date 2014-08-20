2293cda2c956407478b9205f67fdf124fb3882ce
Huangxin Wang <maggiewangsjtu@gmail.com>
2014-08-20 10:07:35 -0400

    new implementation

---
+++ Palindrome-Number.java
@@ -0,0 +1,23 @@
+/**
+ * Problem Statement: Palindrome Number
+ * Link: https://oj.leetcode.com/problems/palindrome-number/
+ * Time: 680 ms
+ * 
+ * Determine whether an integer is a palindrome. Do this without extra space.
+ */
+
+
+public class Solution {
+    public boolean isPalindrome(int x) {
+        
+        String num = Integer.toString(x);
+        
+        for(int i=0; i<num.length()/2; i++){
+            if(num.charAt(i) != num.charAt(num.length()-i-1))
+                return false;
+        }
+        
+        return true;
+        
+    }
+}
