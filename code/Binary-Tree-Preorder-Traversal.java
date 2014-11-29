27b18a5bffedc39c78a57c9d7d94a8ad3c85730c
Huangxin <maggiewangsjtu@gmail.com>
2014-11-29 14:26:31 -0500

    Update Binary-Tree-Preorder-Traversal.java

--- code/Binary-Tree-Preorder-Traversal.java
+++ code/Binary-Tree-Preorder-Traversal.java
@@ -29,3 +29,34 @@ public class Solution {
         
     }
 }
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Round 2: 11/29/2014
+
+//time complexity O(n)
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    
+    ArrayList<Integer> rnt = new ArrayList<Integer>();
+    
+    public ArrayList<Integer> preorderTraversal(TreeNode root) {
+        //special case
+        if(root==null) return rnt;
+        
+        //preorder traversal
+        rnt.add(root.val);
+        if(root.left!=null) preorderTraversal(root.left);
+        if(root.right!=null) preorderTraversal(root.right);
+        
+        return rnt;
+    }
+}
