8bd46ea03185ea650e677ac705ee116b1c26efda
Huangxin <maggiewangsjtu@gmail.com>
2014-11-29 17:44:05 -0500

    Update Word-Break-II.java

--- code/Word-Break-II.java
+++ code/Word-Break-II.java
@@ -46,3 +46,66 @@ public class Solution {
             
     }
 }
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//Round 2: 11/29/2014
+
+/**
+ * Reference: https://stupidcodergoodluck.wordpress.com/2013/11/16/leetcode-word-break-ii/
+ * 
+ */
+
+public class Solution {
+    public List<String> wordBreak(String s, Set<String> dict) {
+        
+        ArrayList<String> rnt = new ArrayList<String>();
+        
+        //use dynamic programming to decide whether the string can is breakable or not
+        
+        //denote f[0..n] to represent whether f[0..n] can achieve wordbreak
+        boolean[] f = new boolean[s.length()+1];
+        f[0] = true;
+        for(int i=1; i<=s.length(); i++){
+            String currWord = s.substring(0,i);
+            if(dict.contains(currWord)){
+                f[i] = true;
+                continue;
+            }else{
+                for(int j=0; j<i; j++){
+                    String word = s.substring(j,i);
+                    if(f[j] && dict.contains(word)){
+                        f[i] = true;
+                        break;
+                    }
+                }
+            }
+        }
+        
+        //cannot break
+        if(!f[s.length()]) return rnt;
+        
+        //dfs 
+        dfs(s, dict, 0, "", rnt);
+        
+        return rnt;
+    }
+    
+    
+    //dfs find all string
+    public void dfs(String s, Set<String> dict, int start, String tmp, ArrayList<String> rnt){
+        
+        if(start>= s.length()){
+            rnt.add(tmp);
+            return;
+        }
+        
+        for(int i=start+1; i<=s.length(); i++){
+            String word = s.substring(start, i);
+            if(dict.contains(word)){
+                String newtmp = tmp.length()==0? word:tmp + " "+word;
+                dfs(s, dict, i, newtmp, rnt);
+            }
+        }
+    }
+}
