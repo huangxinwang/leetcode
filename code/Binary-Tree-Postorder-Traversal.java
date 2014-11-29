178284a0b34350347c1d7c4426d572f7c32d1b2e
Huangxin <maggiewangsjtu@gmail.com>
2014-11-29 14:23:12 -0500

    Update Binary-Tree-Postorder-Traversal.java

--- code/Binary-Tree-Postorder-Traversal.java
+++ code/Binary-Tree-Postorder-Traversal.java
@@ -29,3 +29,36 @@ public class Solution {
         return rntList;
     }
 }
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Round 2: 11/29/2014
+
+//time complexity: O(n)
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    
+    ArrayList<Integer> rnt = new ArrayList<Integer>();
+    
+    
+    public ArrayList<Integer> postorderTraversal(TreeNode root) {
+        //special case
+        if(root==null) return rnt;
+        
+        //post order traverse
+        if(root.left!=null) postorderTraversal(root.left);
+        if(root.right!=null) postorderTraversal(root.right);
+        rnt.add(root.val);
+        
+        return rnt;
+    }
+
+}
