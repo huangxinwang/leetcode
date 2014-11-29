06cea6f24c1f12a81aea83975df0696c88dd81af
Huangxin <maggiewangsjtu@gmail.com>
2014-11-29 15:53:54 -0500

    Update ReorderList.java

--- code/ReorderList.java
+++ code/ReorderList.java
@@ -1,7 +1,7 @@
 /**
  * Problem Statement: Reorder List
- * Given a singly linked list L: L0?L1?…?Ln-1?Ln,
- * reorder it to: L0?Ln?L1?Ln-1?L2?Ln-2?…
+ * Given a singly linked list L: L0?L1?â€¦?Ln-1?Ln,
+ * reorder it to: L0?Ln?L1?Ln-1?L2?Ln-2?â€¦
  * You must do this in-place without altering the nodes' values.
  * 
  * For example,
@@ -73,5 +73,67 @@ public class Solution {
         
         
         
+    }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Round 2: 11/29/2014
+
+//time complexity O(n)
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public void reorderList(ListNode head) {
+        
+        //special case
+        if(head==null || head.next==null) return;
+        
+        //split list into two sublist
+        ListNode fast = head;
+        ListNode slow = head;
+        
+        //find mid as split point
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        ListNode mid = slow;
+        
+        //reverse second half 
+        ListNode first = mid.next;
+        if(first==null) return;
+        ListNode second = first.next;
+        ListNode third  = first.next;
+        first.next = null;
+        while(third!=null){
+            third = third.next;
+            second.next = first;
+            first = second;
+            second = third;
+        }
+        
+        mid.next = null;
+
+        
+        //merge two sublist
+        while(head.next!=null && first!=null){
+            ListNode tmp = head.next;
+            head.next = first;
+            first = first.next;
+            head.next.next = tmp;
+            head = tmp;
+        }
     }
 }
