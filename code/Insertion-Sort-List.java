a06dbbe53f70584918af3967072f93d56eba1d94
Huangxin <maggiewangsjtu@gmail.com>
2014-11-29 12:35:04 -0500

    Update Insertion-Sort-List.java

--- code/Insertion-Sort-List.java
+++ code/Insertion-Sort-List.java
@@ -55,3 +55,58 @@ public class Solution {
 
     }
 }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Round 2: 11/29/2014
+//time complexity O(n^2); space complexity O(n)
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        
+        //use dummyHead to simplify the implementation
+        ListNode dummyHead = new ListNode(-1);
+        //use curr to denote the current scanned position
+        ListNode curr = head;
+        
+        
+        while(curr!=null){
+
+            //if curr is to be insert right after dummyHead
+            if(dummyHead.next==null || dummyHead.next.val>= curr.val){
+                ListNode newNode = new ListNode(curr.val);
+                newNode.next = dummyHead.next;
+                dummyHead.next = newNode;
+                curr = curr.next;
+            }
+            //if curr is to be insert in the list
+            else{
+                ListNode tmp = dummyHead.next;
+                while(tmp.next!=null){
+                    if(tmp.val <= curr.val && curr.val <= tmp.next.val)
+                        break;
+                    else tmp = tmp.next;
+                }
+                
+                //insert curr between tmp and tmp.next
+                ListNode newNode = new ListNode(curr.val);
+                newNode.next = tmp.next;
+                tmp.next = newNode;
+                curr = curr.next;
+            }
+        }
+        
+        return dummyHead.next;
+    }
+}
