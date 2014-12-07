b6837fdc3db1e528b792cdfa28c460825c4a353a
Huangxin <maggiewangsjtu@gmail.com>
2014-12-07 13:39:10 -0500

    Update Triangle.java

--- code/Triangle.java
+++ code/Triangle.java
@@ -42,3 +42,29 @@ public class Solution {
         return min;
     }
 }
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//Round 2: 12/07/2014
+//Space complexity O(n)
+public class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        
+        int len = triangle.size();
+        int[] total = new int[len];
+        
+        //initial total[i] using the last layer
+        for(int i=0; i<len; i++)
+            total[i] = triangle.get(len-1).get(i);
+            
+        for(int i=len-2; i>=0; i--){
+            //traverse i+1 layers to construct the min-value in the i-th layer
+            for(int j=0; j<triangle.get(i+1).size()-1; j++){
+                total[j] = triangle.get(i).get(j) + Math.min(total[j], total[j+1]);
+            }
+        }
+        
+        return total[0];
+    }
+}
