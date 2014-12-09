93d3046bbe72a8ab6d66c315838d28cbe270bf69
Huangxin <maggiewangsjtu@gmail.com>
2014-12-09 14:54:56 -0500

    Update Pascal-Triangle-II.java

--- code/Pascal-Triangle-II.java
+++ code/Pascal-Triangle-II.java
@@ -36,3 +36,31 @@ public class Solution {
         return curr;
     }
 }
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//Round 2: 12/09/2014
+//Time complexity O(n), n is the total elements
+//Space compexity is O(k), k is the number of rows
+public class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        
+        //use two layers
+        //in each interation, curr list is used to buid next list
+        ArrayList<Integer> curr = new ArrayList<Integer>();
+        ArrayList<Integer> next = new ArrayList<Integer>();
+        if(rowIndex<0) return curr;
+        curr.add(1);
+        //note index start from 0
+        for(int i=0; i<rowIndex; i++){
+            next.add(curr.get(0));
+            for(int j=0; j<curr.size()-1; j++)
+                next.add(curr.get(j) + curr.get(j+1));
+            next.add(curr.get(curr.size()-1));
+        curr = new ArrayList<Integer>(next);
+        next = new ArrayList<Integer>();
+        }
+        
+        return curr;
+    }
+}
