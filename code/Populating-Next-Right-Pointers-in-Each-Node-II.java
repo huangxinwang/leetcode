5bb9782df2b001958ccf6775ce58973ad451ce9f
Huangxin <maggiewangsjtu@gmail.com>
2014-12-10 01:08:31 -0500

    Update Populating-Next-Right-Pointers-in-Each-Node-II.java

--- code/Populating-Next-Right-Pointers-in-Each-Node-II.java
+++ code/Populating-Next-Right-Pointers-in-Each-Node-II.java
@@ -42,3 +42,29 @@ public class Solution {
         
     }
 }
+
+
+/////////////////////////////////////////////////////////////////////////////
+//Round 2: 12/10/2014
+public void connect(TreeLinkNode root) {
+        
+        //use BFS
+        //two layers to keep track of current and next
+        ArrayList<TreeLinkNode> curr = new ArrayList<TreeLinkNode>();
+        ArrayList<TreeLinkNode> next = new ArrayList<TreeLinkNode>();
+        if(root==null) return;
+        
+        curr.add(root);
+        while(curr.size()!=0){
+            for(int i=0; i<=curr.size()-1; i++){
+                TreeLinkNode currNode = curr.get(i);
+                if(i<curr.size()-1)
+                    currNode.next = curr.get(i+1);
+                if(currNode.left!=null) next.add(currNode.left);
+                if(currNode.right!=null) next.add(currNode.right);;
+            }
+            curr.get(curr.size()-1).next = null;
+            curr = new ArrayList<TreeLinkNode>(next);
+            next = new ArrayList<TreeLinkNode>();
+        }
+    }
