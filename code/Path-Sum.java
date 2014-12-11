fb1f63728c0aa7a0b39fb7d35f8de63a74076d9c
Huangxin <maggiewangsjtu@gmail.com>
2014-12-11 16:31:39 -0500

    Update Path-Sum.java

--- code/Path-Sum.java
+++ code/Path-Sum.java
@@ -35,3 +35,36 @@ public class Solution {
     }
     
 }
+
+
+///////////////////////////////////////////////////////////////////////////////////////
+//Round 2: 12/11/2014
+//DFS
+
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        //special case
+        if(root==null) return false;
+        //terminate case
+        if(root.left==null && root.right==null && sum-root.val==0) return true;
+        //search left
+        if (root.left!=null){
+            if(hasPathSum(root.left, sum-root.val)) return true;
+        }
+        //search right
+        if(root.right!=null){
+            if(hasPathSum(root.right, sum-root.val)) return true;
+        }
+        
+        return false;
+    }
+}
