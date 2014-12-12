d3958052af6c59420c0284c58a18e46908609b00
Huangxin <maggiewangsjtu@gmail.com>
2014-12-12 02:41:59 -0500

    Update Maximum-Depth-of-Binary-Tree.java

--- code/Maximum-Depth-of-Binary-Tree.java
+++ code/Maximum-Depth-of-Binary-Tree.java
@@ -46,3 +46,57 @@ public class Solution {
         return leftHeight > rightHeight ? leftHeight : rightHeight;
     }
 }
+
+
+////////////////////////////////////////////////////////////////////////
+//Round 2: 12/12/2014
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int minDepth(TreeNode root) {
+        //special case
+        if(root==null) return 0;
+        
+        //use two layers to do BFS traverse
+        ArrayList<TreeNode> curr = new ArrayList<TreeNode>();
+        ArrayList<TreeNode> next = new ArrayList<TreeNode>();
+        
+        //init
+        curr.add(root);
+        int depth = 1;
+        
+        while(curr.size()>0){
+            
+            //construct next layer using curr layer
+            for(int i=0; i<curr.size(); i++){
+                TreeNode node = curr.get(i);
+                //min depth appears when a node has no childern
+                 if(node.left==null && node.right==null) return depth;
+                 
+                 //constructing next layer
+                if(node.left!=null)
+                    next.add(node.left);
+               
+                if(node.right!=null)
+                    next.add(node.right);
+               
+            }
+            
+            //update
+            depth++;
+            curr = new ArrayList<TreeNode>(next);
+            next = new ArrayList<TreeNode>();
+        }
+        
+        return depth;
+    }
+    
+    
+}
