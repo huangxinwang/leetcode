a51939ffef7362eb83fa47e46e2013d8f827ea5f
Huangxin <maggiewangsjtu@gmail.com>
2014-12-13 15:13:42 -0500

    Update Balanced-Binary-Tree.java

--- code/Balanced-Binary-Tree.java
+++ code/Balanced-Binary-Tree.java
@@ -56,3 +56,42 @@ public class Solution {
     
    
 }
+
+
+///////////////////////////////////////////////////////////////////////////
+//Round 2: 12/13/2014
+/**
+ * Ref: http://fisherlei.blogspot.com/2013/01/leetcode-balanced-binary-tree-solution.html
+ * 
+ */
+
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root==null) return true;
+        int val = getBalance(root);
+        if(val==-1) return false;
+        return true;
+    }
+    
+    public int getBalance(TreeNode root){
+        //special case
+        if(root==null) return 0;
+        //check whether left and right subtree is balanced
+        int leftVal = getBalance(root.left);
+        int rightVal = getBalance(root.right);
+        //not balanced
+        if(leftVal==-1 || rightVal==-1) return -1;
+        if(leftVal>rightVal+1 || rightVal>leftVal+1) return -1;
+        //balanced
+        else return leftVal>rightVal ? leftVal+1 : rightVal+1;
+    }
+}
