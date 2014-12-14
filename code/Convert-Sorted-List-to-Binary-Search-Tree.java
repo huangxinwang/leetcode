962849312ffc098593f61fee9a24d8adec75163c
Huangxin <maggiewangsjtu@gmail.com>
2014-12-13 20:38:22 -0500

    Update Convert-Sorted-List-to-Binary-Search-Tree.java

--- code/Convert-Sorted-List-to-Binary-Search-Tree.java
+++ code/Convert-Sorted-List-to-Binary-Search-Tree.java
@@ -72,3 +72,66 @@ public class Solution {
         return root;
     }
 }
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//Round 2: 12/13/2014
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        
+        if(head==null) return null;
+        if(head.next==null) return new TreeNode(head.val);
+        
+        //count total length
+        int len = 1;
+        ListNode countnode = head;
+        while(countnode.next!=null){
+            len++;
+            countnode = countnode.next;
+        }
+        
+        //find mid
+        ListNode midnode = head;
+        int mid = len/2;
+        while(mid>0){
+            midnode = midnode.next;
+            mid--;
+        }
+        
+        //split to firsthead and secondhead
+        TreeNode rnt = new TreeNode(midnode.val);
+        mid = len/2;
+        ListNode firstHead = head;
+        while(mid>1){
+            firstHead = firstHead.next;
+            mid--;
+        }
+        firstHead.next=null;
+        
+        //left subtree exist only when there are more than 2 elements in the list
+        //recursive convert left and right subtree
+        if(len>=2)
+            rnt.left = sortedListToBST(head);
+        ListNode secondHead = midnode.next;
+        rnt.right = sortedListToBST(secondHead);
+        
+        
+        return rnt;
+    }
+}
