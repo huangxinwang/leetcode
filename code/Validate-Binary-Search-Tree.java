ea3119e8458a9b1ec3fdc716f43d3d9a51671b41
Huangxin <maggiewangsjtu@gmail.com>
2014-12-15 18:15:31 -0500

    Update Validate-Binary-Search-Tree.java

--- code/Validate-Binary-Search-Tree.java
+++ code/Validate-Binary-Search-Tree.java
@@ -35,3 +35,40 @@ public class Solution {
         return false;
     }
 }
+
+///////////////////////////////////////////////////////////////////////////
+//Round 2: 12/15/2014
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isValidBST(TreeNode root) {
+        
+        if(root==null) return true;
+        return helper(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+    
+    public boolean helper(TreeNode root, int low, int high){
+        
+        if(root==null) return true;
+
+        //false cases
+        if(root.val == Integer.MIN_VALUE && root.left!=null) return false;
+        if(root.val == Integer.MAX_VALUE && root.right!=null) return false;
+
+        if(low> root.val || root.val > high) return false;
+        
+        //recursive
+        //update the upper bound of left tree, and lower bound of upper tree; 
+        //to avoid the case that the node value equal to Integer.MIN_VALUE
+        return helper(root.left, low, root.val-1) && helper(root.right, root.val+1, high);
+       
+     
+    }
+}
