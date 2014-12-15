f1d85a0d8bec2005442c0b6c676825cf46d73062
Huangxin <maggiewangsjtu@gmail.com>
2014-12-15 02:15:37 -0500

    Update Binary-Tree-Zigzag-Level-Order-Traversal.java

--- code/Binary-Tree-Zigzag-Level-Order-Traversal.java
+++ code/Binary-Tree-Zigzag-Level-Order-Traversal.java
@@ -62,3 +62,54 @@ public class Solution {
         
     }
 }
+
+//////////////////////////////////////////////////////////////////////////
+//Round 2: 12/15/2014
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ArrayList<ArrayList<Integer>> zigzagLevelOrder(TreeNode root) {
+        
+        //two layer BFS traverse
+        ArrayList<TreeNode> curr = new ArrayList<TreeNode>();
+        ArrayList<TreeNode> next = new ArrayList<TreeNode>();
+        curr.add(root);
+        ArrayList<ArrayList<Integer>> rnt = new ArrayList<ArrayList<Integer>>();
+        if(root==null) return rnt;
+        //keep a flag to decide the order of traverse, i.e., start to end, or end to start
+        boolean reverse = false;
+        
+        //construct next layer using current layer
+        //and also keep record of the value of the node to put to the return result
+        while(curr.size()>0){
+            ArrayList<Integer> currVal = new ArrayList<Integer>();
+            for(int i=0; i<curr.size(); i++){
+                TreeNode node = curr.get(i);
+                if(reverse)
+                    currVal.add(0,node.val);
+                else 
+                    currVal.add(node.val);
+                if(node.left!=null)
+                    next.add(node.left);
+                if(node.right!=null)
+                    next.add(node.right);
+            }
+            
+            //update curr and next layer
+            rnt.add(new ArrayList<Integer>(currVal));
+            curr = new ArrayList<TreeNode>(next);
+            next = new ArrayList<TreeNode>();
+            if(reverse) reverse = false;
+            else reverse = true;
+        }
+        
+        return rnt;
+    }
+}
