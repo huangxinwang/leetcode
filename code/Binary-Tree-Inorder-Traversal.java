d4e92e2b0863d4f24747858a9c1453551eb44d81
Huangxin <maggiewangsjtu@gmail.com>
2014-12-17 12:51:30 -0500

    Update Binary-Tree-Inorder-Traversal.java

--- code/Binary-Tree-Inorder-Traversal.java
+++ code/Binary-Tree-Inorder-Traversal.java
@@ -33,3 +33,31 @@ public class Solution {
     }
     
 }
+
+
+/////////////////////////////////////////////////////////////
+//Round 2: 12/17/2014
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    
+    ArrayList<Integer> rnt = new ArrayList<Integer>();
+    
+    public List<Integer> inorderTraversal(TreeNode root) {
+       
+        if(root==null) return rnt;
+        if(root.left!=null)
+            inorderTraversal(root.left);
+        rnt.add(root.val);
+        if(root.right!=null)
+            inorderTraversal(root.right);
+        return rnt;
+    }
+}
