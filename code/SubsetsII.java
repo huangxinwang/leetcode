5a288947fe1eb01ef144890247f4b5593d890ab7
Huangxin <maggiewangsjtu@gmail.com>
2014-12-17 21:01:24 -0500

    Update SubsetsII.java

--- code/SubsetsII.java
+++ code/SubsetsII.java
@@ -50,3 +50,50 @@ public class Solution {
         }
     }
 }
+
+
+/////////////////////////////////////////////////////////////////////////////
+//Round 2: 12/17/2014
+public class Solution {
+    public ArrayList<ArrayList<Integer>> subsetsWithDup(int[] num) {
+        
+        ArrayList<ArrayList<Integer>> rnt = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> tmp = new ArrayList<Integer>();
+        
+        Arrays.sort(num);
+        
+        //enumerate different possible length of subset
+        //dfs enumerate
+        for(int i=1; i<= num.length; i++){
+            dfs(i, 0, rnt, tmp, num);
+        }
+        
+        
+        
+        rnt.add(new ArrayList<Integer>());
+        return rnt;
+    }
+    
+    public void dfs(int depth, int start, ArrayList<ArrayList<Integer>> rnt, ArrayList<Integer> tmp, int[] num){
+        
+        if(tmp.size()==depth){
+            rnt.add(new ArrayList<Integer>(tmp));
+            return;
+        }
+        
+        ArrayList<Integer> existNum = new ArrayList<Integer>();
+        
+        for(int i=start; i<num.length; i++){
+            
+            //this is different from num[i-1]==num[i]
+            //since this is dfs procedure
+            if(existNum.contains(num[i])) continue;
+            
+            
+            existNum.add(num[i]);
+            tmp.add(num[i]);
+            dfs(depth, i+1, rnt, tmp, num);
+            tmp.remove(tmp.size()-1);
+        }
+    }
+}
