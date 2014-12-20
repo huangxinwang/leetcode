020443c8d1bf7bc7e30e1ac69cb1faa478dd8344
Huangxin <maggiewangsjtu@gmail.com>
2014-12-19 19:54:24 -0500

    Update Remove-Duplicates-from-Sorted-ListII.java

--- code/Remove-Duplicates-from-Sorted-ListII.java
+++ code/Remove-Duplicates-from-Sorted-ListII.java
@@ -62,3 +62,58 @@ public class Solution {
         
     }
 }
+
+
+////////////////////////////////////////////////////////////
+//Round 2: 12/19/2014
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        
+        //special case
+        if(head==null) return head;
+        
+        //create a dummyHead, in case the first element needs to be deleted
+        ListNode dummyHead = new ListNode(head.val-1);
+        
+        //pointer to get new valid elements
+        ListNode pre = dummyHead;
+        
+        //pointer to traverse the list
+        ListNode curr = head;
+        
+        
+        while(curr!=null && curr.next!=null){
+            
+            //skip duplicate elements
+            if(curr.val==curr.next.val){
+                int del = curr.val;
+                while(curr.val==del){
+                    curr = curr.next;
+                    if(curr==null) return dummyHead.next;
+                }
+            }
+            //accept valid elements
+            else{
+                pre.next = curr;
+                curr = curr.next;
+                pre = pre.next;
+                pre.next = null;
+            }
+        }
+        
+        //need to accept the valid elements when there is only one element
+        pre.next = curr;
+        return dummyHead.next;
+    }
+}
