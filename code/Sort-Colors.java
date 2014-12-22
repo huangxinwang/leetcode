686c3d2fdc57e48390ccf2a364bd3b988d2cf9e8
Huangxin <maggiewangsjtu@gmail.com>
2014-12-22 10:00:17 -0500

    Update Sort-Colors.java

--- code/Sort-Colors.java
+++ code/Sort-Colors.java
@@ -36,3 +36,27 @@ public class Solution {
         }
     }
 }
+
+///////////////////////////////////////////////////////////////
+//Round 2: 12/22/2014
+public class Solution {
+    public void sortColors(int[] A) {
+        int red = 0;
+        int white = 0;
+        int blue = 0;
+        
+        //count the number of times of appearance of red, white, blue respectively
+        for(int i=0; i<A.length; i++){
+            if(A[i]==0) red++;
+            if(A[i]==1) white++;
+            if(A[i]==2) blue++;
+        }
+        
+        for(int i=0; i<red; i++)
+            A[i] = 0;
+        for(int i=red; i<red+white; i++)
+            A[i] = 1;
+        for(int i=red+white; i<red+white+blue; i++)
+            A[i] = 2;
+    }
+}
