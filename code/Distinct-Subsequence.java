b409e06f5c418dbd8846fa685e904ee42de57391
Huangxin <maggiewangsjtu@gmail.com>
2014-12-23 12:11:53 -0500

    Update Distinct-Subsequence.java

--- code/Distinct-Subsequence.java
+++ code/Distinct-Subsequence.java
@@ -44,3 +44,38 @@ public class Solution {
         
     }
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+//Round 2: 12/23/2014
+public class Solution {
+    public int numDistinct(String S, String T) {
+        
+        //dynamic programming
+        //num[i][j] means the number of match of string S[0...i], T[0..j]
+        int[][] num = new int[S.length()+1][T.length()+1];
+        
+        //initialization
+        for(int i=0; i<=S.length(); i++){
+            num[i][0] = 1;
+        }
+        
+        for(int j=0; j<T.length(); j++){ 
+            for(int i=0; i<S.length(); i++)
+           {
+               if(j>i){
+                   continue;
+               }
+                if(S.charAt(i) == T.charAt(j)){
+                    //if current char equal, then the number of match of (i+1)(j+1) 
+                    //is the sum of the match of(i)(j) and the match of (i)(j+1)
+                    num[i+1][j+1] = num[i][j] + num[i][j+1];
+                }
+                else
+                    //otherwise, it is the same as if (i)character is not scaned
+                    num[i+1][j+1] = num[i][j+1];
+            }
+        }
+        
+        return num[S.length()][T.length()];
+    }
+}
