26e5146b546107137b01247650e94a39f7639ecc
Huangxin <maggiewangsjtu@gmail.com>
2014-12-24 21:47:31 -0500

    Update Unique-Paths-II.java

--- code/Unique-Paths-II.java
+++ code/Unique-Paths-II.java
@@ -64,3 +64,41 @@ public class Solution {
         return num[m-1][n-1];
     }
 }
+
+//////////////////////////////////////////////////////////////////////////
+//Round 2: 12/24/2014
+public class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        
+        int row = obstacleGrid.length;
+        int col = obstacleGrid[0].length;
+        
+        //define dynamic table
+        int[][] path = new int[row][col];
+        
+        //initialize the table
+        path[0][0] = obstacleGrid[0][0]==0? 1:0;
+        
+        //set entry to 0 if its adjacent is 0, or it contains obstable
+        for(int i=1; i<row; i++){
+            path[i][0] = path[i-1][0]==0? 0:1;
+            if(obstacleGrid[i][0]==1) path[i][0] = 0;
+            
+        }
+        
+        //dynamic build the table
+        for(int i=1; i<col; i++){
+            path[0][i] = path[0][i-1] ==0 ? 0:1;
+            if(obstacleGrid[0][i]==1)  path[0][i]=0;
+        }
+        
+        for(int i=1; i<row; i++){
+            for(int j=1; j<col; j++){
+                path[i][j] = path[i-1][j] + path[i][j-1];
+                if(obstacleGrid[i][j]==1) path[i][j] = 0;
+            }
+        }
+        
+        return path[row-1][col-1];
+    }
+}
