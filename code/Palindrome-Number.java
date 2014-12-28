fdd6a40129f241576ac8470d1dd260de7fa453ec
Huangxin <maggiewangsjtu@gmail.com>
2014-12-28 09:30:40 -0500

    Update Palindrome-Number.java

--- code/Palindrome-Number.java
+++ code/Palindrome-Number.java
@@ -21,3 +21,20 @@ public class Solution {
         
     }
 }
+
+//////////////////////////////////////////////////
+//Round 2: 12/28/2014
+public class Solution {
+    public boolean isPalindrome(int x) {
+        
+        String num = String.valueOf(x);
+        
+        for(int i=0; i<=num.length()/2; i++){
+            if(num.charAt(i) != num.charAt(num.length()-i-1))
+                return false;
+        }
+        
+        return true;
+        
+    }
+}
