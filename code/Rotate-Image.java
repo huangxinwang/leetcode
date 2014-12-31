3ca1e62dd39a0e1f93eb0e1851bdcb061fd97ce9
Huangxin <maggiewangsjtu@gmail.com>
2014-12-30 19:06:36 -0500

    Update Rotate-Image.java

--- code/Rotate-Image.java
+++ code/Rotate-Image.java
@@ -30,3 +30,24 @@ public class Solution {
         
     }
 }
+
+//////////////////////////////////////////////////////////////////
+//Round 2: 12/30/2014
+public class Solution {
+    public void rotate(int[][] matrix) {
+        
+        int n = matrix.length;
+        
+        //use the logic: matrix[i][j] = matrix[n-j-1][i]
+        for(int i=0; i<(n+1)/2; i++){
+            for(int j=0; j<n/2; j++){
+              
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[n-j-1][i];
+                matrix[n-j-1][i] = matrix[n-i-1][n-j-1];
+                matrix[n-i-1][n-j-1] = matrix[j][n-i-1];
+                matrix[j][n-i-1] = tmp;
+            }
+        }
+    }
+}
