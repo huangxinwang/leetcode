1a65632301667d252d7ef80df9bbf805adf334d8
Huangxin <maggiewangsjtu@gmail.com>
2015-01-01 13:12:49 -0500

    Update Substring-with-Concatenation-of-All-Words.java

--- code/Substring-with-Concatenation-of-All-Words.java
+++ code/Substring-with-Concatenation-of-All-Words.java
@@ -90,3 +90,71 @@ public class Solution {
         
     }
 }
+
+
+////////////////////////////////////////////////////////////////////////////
+//Round 2： 1/1/2015
+public class Solution {
+    public ArrayList<Integer> findSubstring(String S, String[] L) {
+        
+        ArrayList<Integer> rnt = new ArrayList<Integer>();
+        
+        //create dict for L
+        HashMap<String, Integer> dict = new HashMap<String, Integer>();
+        for(int i=0; i<L.length; i++){
+            if(dict.containsKey(L[i]))
+                dict.put(L[i], dict.get(L[i])+1);
+            else
+                dict.put(L[i],1);
+        }
+        
+        int wordLen = L[0].length();
+        
+        //for each possible start of string S
+        for(int i=0; i<wordLen; i++){
+            
+            HashMap<String, Integer> currDict = new HashMap<String, Integer>();
+            int count = 0;
+            int start = i;
+            
+            //check each word, in step of wordLen
+            for(int j=i; j<=S.length()-wordLen; j+=wordLen){
+                String word = S.substring(j, j+wordLen);
+                
+                //reset the strat, dict, count
+                if(!dict.containsKey(word)){
+                    currDict.clear();
+                    count = 0;
+                    start = j+ wordLen;
+                }
+                else{
+                    //add word to currDict
+                    if(currDict.containsKey(word))
+                        currDict.put(word, currDict.get(word)+1);
+                    else
+                        currDict.put(word,1);
+                    count++;
+                    
+                    //current window is not valid, advance the front of the window
+                    while(currDict.get(word) > dict.get(word)){
+                        String headWord = S.substring(start, start+wordLen);
+                        currDict.put(headWord, currDict.get(headWord)-1);
+                        start+=wordLen;
+                        count--;
+                    }
+                    
+                    //curr window is valid, advance the front of the window
+                    if(count==L.length){
+                        rnt.add(start);
+                        String headWord = S.substring(start, start+wordLen);
+                        currDict.put(headWord, currDict.get(headWord)-1);
+                        count--;
+                        start += wordLen;
+                    }
+                }
+            }
+        }
+        
+        return rnt;
+    }
+}
