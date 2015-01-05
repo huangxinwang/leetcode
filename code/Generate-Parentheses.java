aeb49b5c39e20ea63e0c906762588502e6782782
Huangxin <maggiewangsjtu@gmail.com>
2015-01-05 14:35:46 -0500

    Update Generate-Parentheses.java

--- code/Generate-Parentheses.java
+++ code/Generate-Parentheses.java
@@ -44,3 +44,41 @@ public class Solution {
       
     }
 }
+
+
+/////////////////////////////////////////////////////////////////////////////
+//Round 2: 1/5/2015
+public class Solution {
+    public ArrayList<String> generateParenthesis(int n) {
+        
+        ArrayList<String> rnt = new ArrayList<String>();
+        int left = 0;
+        int right = 0;
+        String tmp = new String();
+        dfs(left, right, n,rnt, tmp);
+        
+        return rnt;
+    }
+    
+    public void dfs(int left, int right, int n, ArrayList<String> rnt, String tmp){
+        
+        if(left+right==2*n)
+        {
+            rnt.add(tmp);
+            return;
+        }
+        
+        //add right
+        if(right<left){
+            String newTmp = tmp + ")";
+            dfs(left, right+1, n, rnt, newTmp);
+        }
+        
+        //add left
+        if(left<n)
+        {
+            String newTmp = tmp + "(";
+            dfs(left+1, right, n, rnt, newTmp);
+        }
+    }
+}
