08f1142734b338d3be323681e65282bced4e7bdc
Huangxin <maggiewangsjtu@gmail.com>
2015-01-07 16:11:49 -0500

    Update Integer-to-Roman.java

--- code/Integer-to-Roman.java
+++ code/Integer-to-Roman.java
@@ -52,3 +52,48 @@ public class Solution {
         return rnt;
     }
 }
+
+/////////////////////////////////////////////////////////////////////////
+//Round 2: 1/7/2015
+public class Solution {
+    
+    public String intToRoman(int num) {
+        
+        char[] roman = new char[]{'I', 'V', 'X','L','C','D','M'};
+        String rnt = "";
+        int scale = 1000;
+        
+        //divide scale
+        //decide result to be the following cases: 0-3; 4; 5-8; 9
+        for(int i=6; i>=0; i-=2){
+            int digit = num/scale;
+            
+            if(digit<=3){
+                for(int j=0; j<digit; j++)
+                    rnt += roman[i];
+            }
+            
+            if(digit==4){
+                rnt+= roman[i];
+                rnt += roman[i+1];
+            }
+            
+            if(digit>=5 && digit <=8){
+                    rnt += roman[i+1];
+                for(int j=5; j<digit; j++)
+                    rnt += roman[i];
+            }
+            
+            if(digit==9){
+                rnt += roman[i];
+                rnt += roman[i+2];
+            }
+            
+            num = num%scale;
+            scale = scale/10;
+            }
+            
+            return rnt;
+        }
+        
+}
