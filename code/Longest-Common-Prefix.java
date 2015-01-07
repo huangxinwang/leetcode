7d36412195ed31af40c094fd2946fe81c71c7974
Huangxin <maggiewangsjtu@gmail.com>
2015-01-07 10:14:52 -0500

    Update Longest-Common-Prefix.java

--- code/Longest-Common-Prefix.java
+++ code/Longest-Common-Prefix.java
@@ -39,3 +39,35 @@ public class Solution {
     
     
 }
+
+
+/////////////////////////////////////////////////////////
+//Round 2: 1/7/2015
+//similar merge k-sorted list
+
+public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        
+        if(strs.length==0) return new String();
+        return helper(strs, 0, strs.length-1);
+    }
+    
+    public String helper(String[] strs, int start, int end){
+        if(start==end) return strs[start];
+        if(start+1 == end) return commonPrefix(strs[start], strs[end]);
+        int mid = (start+end)/2;
+        //recursive using helper to get string
+        return commonPrefix(helper(strs, start, mid), helper(strs, mid+1, end));
+    }
+    
+    //find commonprefix of two string
+    public String commonPrefix(String s1, String s2){
+        int len = 0;
+        for(int i=0; i<Math.min(s1.length(), s2.length()); i++){
+            if(s1.charAt(i) == s2.charAt(i)) len++;
+            else return s1.substring(0,len);
+        }
+        
+        return s1.substring(0,len);
+    }
+}
