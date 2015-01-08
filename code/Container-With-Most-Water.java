2ba33a18ea3337ad20457c3b5b667b8771c0cee4
Huangxin <maggiewangsjtu@gmail.com>
2015-01-08 00:38:42 -0500

    Update Container-With-Most-Water.java

--- code/Container-With-Most-Water.java
+++ code/Container-With-Most-Water.java
@@ -33,3 +33,25 @@ public class Solution {
        return max;
     }
 }
+
+
+///////////////////////////////////////////////
+//Round 2: 1/8/2015
+public class Solution {
+    public int maxArea(int[] height) {
+        
+        int max = 0;
+        int start = 0;
+        int end = height.length-1;
+        while(start<end){
+            int curr = Math.min(height[start], height[end])*(end-start);
+            max = max>curr?max : curr;
+            //logic
+            if(height[start] < height[end])
+                start++;
+            else end--;
+        }
+        
+        return max;
+    }
+}
