cb5aefc5194bb829f46d02b81724026f08138d3b
Huangxin <maggiewangsjtu@gmail.com>
2015-01-18 00:08:22 -0500

    Update a-b-problem.java

--- lint-code/code/a-b-problem.java
+++ lint-code/code/a-b-problem.java
@@ -1,3 +1,24 @@
-// Problem Statement: 
-//For given numbers a and b in function aplusb, return the sum of them.
-//Note: do not use + operation
+/**
+ * Problem: A+B Problem
+ * Ref: http://bubuko.com/infodetail-553698.html
+ * 
+ */
+class Solution {
+    /*
+     * param a: The first integer
+     * param b: The second integer
+     * return: The sum of a and b
+     */
+    public int aplusb(int a, int b) {
+        // Click submit, you will get Accepted!
+        //return a + b;
+        int carry = 0;
+        while(b!=0){
+            carry = a&b;
+            a = a^b;
+            b = carry << 1;
+        }
+        
+        return a;
+    }
+};
