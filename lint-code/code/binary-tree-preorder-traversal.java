25a2b48026727206fc9f2be5f750a1702a4187e8
Huangxin <maggiewangsjtu@gmail.com>
2015-01-20 14:02:54 -0500

    Update binary-tree-preorder-traversal.java

--- lint-code/code/binary-tree-preorder-traversal.java
+++ lint-code/code/binary-tree-preorder-traversal.java
@@ -38,3 +38,47 @@ public class Solution {
             
     }
 }
+
+
+///////////////////////////////////////////////////
+//Approach 2: Iterative 
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: Preorder in ArrayList which contains node values.
+     */
+    public ArrayList<Integer> preorderTraversal(TreeNode root) {
+        // write your code here
+        ArrayList<Integer> rnt = new ArrayList<Integer>();
+        if(root==null) return rnt;
+        
+        //use a stack to store the elements that will be visited in the future
+        Stack<TreeNode> stk = new Stack<TreeNode>();
+        stk.push(root);
+        
+        //note need to push right child before left child
+        //since stack will pop left child first
+        while(!stk.isEmpty()){
+            TreeNode node = stk.pop();
+            rnt.add(node.val);
+            if(node.right!=null)
+                stk.push(node.right);
+            if(node.left != null)
+                stk.push(node.left);
+        }
+        
+        return rnt;
+    }
+}
