3f601bca2c72071c4a87fd4e11c0af6a2bb8127c
Huangxin <maggiewangsjtu@gmail.com>
2015-01-20 16:01:35 -0500

    Update validate-binary-search-tree.java

--- lint-code/code/validate-binary-search-tree.java
+++ lint-code/code/validate-binary-search-tree.java
@@ -31,6 +31,13 @@ public class Solution {
     
     public boolean help(TreeNode root, int min, int max){
         if(root.val > max || root.val <min) return false;
+        
+        //special case
+        //Note: the test cases in lintcode is not complete, it doesn't check the following cases
+        if(root.val == Integer.MAX_VALUE && root.right!=null) return false;
+        if(root.val == Integer.MIN_VALUE && root.left !=null) return false;
+        
+        
         if(root.left !=null){
             if( !help(root.left, min, root.val-1)) return false;
         }
