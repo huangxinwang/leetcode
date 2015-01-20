3ccc4be3c1834eb024aeab039cd35c1f3c26e9bc
Huangxin <maggiewangsjtu@gmail.com>
2015-01-20 14:54:56 -0500

    Create binary-tree-zigzag-level-order-traversal.java

---
+++ lint-code/code/binary-tree-zigzag-level-order-traversal.java
@@ -0,0 +1,64 @@
+/**
+ * Problem Statement: Binary Tree Zigzag Level Order Traversal
+ * 
+ * 
+ */
+
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+ 
+ 
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: A list of lists of integer include 
+     *          the zigzag level order traversal of its nodes' values 
+     */
+    public ArrayList<ArrayList<Integer>> zigzagLevelOrder(TreeNode root) {
+        // write your code here
+        ArrayList<ArrayList<Integer>> rnt = new ArrayList<ArrayList<Integer>>();
+        if(root==null) return rnt;
+        
+        ArrayList<TreeNode> curr = new ArrayList<TreeNode>();
+        curr.add(root);
+        boolean reverse = false;
+        
+        while(curr.size()!=0){
+            ArrayList<TreeNode> nextLayer = new ArrayList<TreeNode>();
+            ArrayList<Integer> currVal = new ArrayList<Integer>();
+            
+            for(int i=0; i<curr.size(); i++){
+                TreeNode node = curr.get(i);
+                currVal.add(node.val);
+                if(node.left!=null) nextLayer.add(node.left);
+                if(node.right!=null) nextLayer.add(node.right);
+            }
+            
+            //reverse the list val if rever is true
+            if(reverse==true){ 
+                Collections.reverse(currVal);
+            }
+            rnt.add(currVal);
+            
+            //change reverse
+            if(reverse == true) 
+                reverse = false;
+            else reverse = true;
+            
+            //update curr layer
+            curr = nextLayer;
+        }
+        
+        return rnt;
+    }
+}
