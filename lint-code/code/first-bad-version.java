6a86cd333029142b6c935dd8124b22530dff189a
Huangxin <maggiewangsjtu@gmail.com>
2015-01-21 18:17:39 -0500

    Update first-bad-version.java

--- lint-code/code/first-bad-version.java
+++ lint-code/code/first-bad-version.java
@@ -25,3 +25,41 @@ class Solution {
         else return help(mid+1, r, n);
     }
 }
+
+
+//////////////////////////////////////////////////
+//Approach 2: Iterative
+
+/**
+ * public class VersionControl {
+ *     public static boolean isBadVersion(int k);
+ * }
+ * you can use VersionControl.isBadVersion(k) to judge wether 
+ * the kth code version is bad or not.
+*/
+class Solution {
+    /**
+     * @param n: An integers.
+     * @return: An integer which is the first bad version.
+     */
+     
+    //iterative
+    public int findFirstBadVersion(int n) {
+        // write your code here
+        int l = 0;
+        int r = n;
+        while(l <r){
+            int mid = l + (r-l)/2;
+            if(VersionControl.isBadVersion(mid))
+                r = mid;
+            else
+                l = mid+1;
+        }
+        
+        return r;
+        
+    }
+    
+   
+}
+
