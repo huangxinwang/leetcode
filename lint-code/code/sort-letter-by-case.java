ef22fc221b96b963f480c8b0004f3d80fdcee583
Huangxin <maggiewangsjtu@gmail.com>
2015-01-24 12:58:34 -0500

    Update sort-letter-by-case.java

--- lint-code/code/sort-letter-by-case.java
+++ lint-code/code/sort-letter-by-case.java
@@ -8,10 +8,11 @@ public class Solution {
         int l = 0;
         int r = chars.length-1;
         
+        //Note A-Z comes before a-z
         while(l <= r){
-            while(l<chars.length-1 && chars[l]<='z')
+            while(l<chars.length-1 && chars[l]<='z' && chars[l]>='a')
                 l++;
-            while(r>0 && chars[r] >='A')
+            while(r>0 && chars[r] >='A' && chars[r]<='Z')
                 r--;
             if(l<=r){
                 char tmp = chars[l];
@@ -23,3 +24,5 @@ public class Solution {
         }
     }
 }
+
+
