534d4150330f2761071908874aa2f1749bb7423c
Huangxin <maggiewangsjtu@gmail.com>
2015-01-28 11:11:15 -0500

    Update digit-counts.java

--- lint-code/code/digit-counts.java
+++ lint-code/code/digit-counts.java
@@ -21,3 +21,28 @@ class Solution {
     
 };
 
+
+//////////////////////////////////////////////////
+//Approach 2: mode 10
+class Solution {
+    /*
+     * param k : As description.
+     * param n : As description.
+     * return: An integer denote the count of digit k in 1..n
+     */
+    public int digitCounts(int k, int n) {
+        // write your code here
+        int count = 0;
+        for(int i=1; i<=n; i++)
+            count = count + helpCount(k,i);
+        return count;
+    }
+    
+    public int helpCount(int k, int n){
+        int count = 0;
+        if(n%10==k) count++;
+        if(n/10>0)
+            count += helpCount(k, n/10);
+        return count;
+    }
+};
