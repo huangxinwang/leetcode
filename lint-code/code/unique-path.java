53d994b6cd1e70947cf8ae99f65218a07d58dc08
Huangxin <maggiewangsjtu@gmail.com>
2015-01-28 20:24:15 -0500

    Update unique-path.java

--- lint-code/code/unique-path.java
+++ lint-code/code/unique-path.java
@@ -1,3 +1,4 @@
+//// 2D
 public class Solution {
     /**
      * @param n, m: positive integer (1 <= n ,m <= 100)
@@ -25,3 +26,29 @@ public class Solution {
         return ways[m-1][n-1];
     }
 }
+
+/////////////////////////////////////////////////////////////////
+// 1D: 
+public class Solution {
+    /**
+     * @param n, m: positive integer (1 <= n ,m <= 100)
+     * @return an integer
+     */
+    public int uniquePaths(int m, int n) {
+        // write your code here 
+        int[] ways = new int[n];
+        
+        for(int i=0; i<n; i++)
+            ways[i] = 1;
+            
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++)
+                ways[j] = ways[j-1]+ways[j];
+        }
+        
+        return ways[n-1];
+       
+    }
+}
+
+
