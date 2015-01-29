407881dfb15cbe15a1086640f96075ae9d9ffaff
Huangxin <maggiewangsjtu@gmail.com>
2015-01-28 21:00:37 -0500

    Create minimum-path-sum.java

---
+++ lint-code/code/minimum-path-sum.java
@@ -0,0 +1,30 @@
+public class Solution {
+    /**
+     * @param grid: a list of lists of integers.
+     * @return: An integer, minimizes the sum of all numbers along its path
+     */
+    public int minPathSum(int[][] grid) {
+        // write your code here
+        int row = grid.length;
+        int col = grid[0].length;
+        
+        int[][] sum = new int[row][col];
+        sum[0][0] = grid[0][0];
+        for(int i=1; i<row; i++){
+            sum[i][0] = sum[i-1][0] + grid[i][0];
+        }
+        
+        for(int i=1; i<col; i++)
+            sum[0][i] = sum[0][i-1] + grid[0][i];
+            
+        
+        for(int i=1; i<row; i++){
+            for(int j=1; j<col; j++){
+                sum[i][j] = Math.min(sum[i-1][j], sum[i][j-1]) + grid[i][j];
+            }
+        }
+        
+        return sum[row-1][col-1];
+    }
+}
+
