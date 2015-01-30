ff95b3508ef3f3acdd6653d9c53a88132d286335
Huangxin <maggiewangsjtu@gmail.com>
2015-01-30 10:59:49 -0500

    Update k-sum.java

--- lint-code/code/k-sum.java
+++ lint-code/code/k-sum.java
@@ -33,3 +33,44 @@ public class Solution {
         }
     }
 }
+
+//////////////////////////////////////////////////////
+//Approach 2: k sum
+public class Solution {
+    /**
+     * @param A: an integer array.
+     * @param k: a positive integer (k <= length(A))
+     * @param target: a integer
+     * @return an integer
+     */
+    public int  kSum(int A[], int k, int target) {
+        // write your code here
+        int[][][] count = new int[A.length+1][k+1][target+1];
+        
+        //initialized  
+        for(int i=1; i<=A.length; i++){
+            if(A[i-1]<=target){
+                for(int j=i; j<=A.length; j++){
+                        count[j][1][A[i-1]] = 1;
+                }
+            }
+        }
+        
+        for(int j=2; j<=k; j++){
+                for(int i=j; i<=A.length; i++){
+                    for(int m=1; m<=target; m++){
+                    
+                    //dynamic updat the table
+                    int remain = m - A[i-1];
+                    if(remain>=0){
+                        //the j number contains A[i-1] or not
+                        count[i][j][m] = count[i-1][j-1][remain] + count[i-1][j][m];
+                    }
+                    else count[i][j][m] = count[i-1][j][m];
+                }
+            }
+        }
+        
+        return count[A.length][k][target];
+    }
+}
