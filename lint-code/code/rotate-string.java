6fe272a8634fb9e27e72469b299ce423699da902
Huangxin <maggiewangsjtu@gmail.com>
2015-02-05 13:23:32 -0500

    Create rotate-string.java

---
+++ lint-code/code/rotate-string.java
@@ -0,0 +1,34 @@
+/**
+ * Ref: http://www.ninechapter.com/problem/55/
+ * 
+ */
+
+public class Solution {
+    /*
+     * param A: A string
+     * param offset: Rotate string with offset.
+     * return: Rotated string.
+     */
+    public char[] rotateString(char[] A, int offset) {
+        // wirte your code here
+        if(A.length==0) return A;
+        
+        offset = offset%(A.length);
+        if(offset==0) return A;
+        
+        //reverse together
+        reverse(A, 0, A.length-1);
+        //reverse seperately
+        reverse(A, 0, offset-1);
+        reverse(A, offset, A.length-1);
+        return A;
+    }
+    
+    public void reverse(char[] A, int low, int high){
+        for(int i=low; i<=(low+high)/2; i++){
+            char tmp = A[i];
+            A[i] = A[high+low-i];
+            A[high+low-i] = tmp;
+        }
+    }
+};
