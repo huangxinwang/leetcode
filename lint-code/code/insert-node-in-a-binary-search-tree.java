3a04ab90fe3e9facdfa094c38c48b7903653ea57
Huangxin <maggiewangsjtu@gmail.com>
2015-02-09 15:59:10 -0500

    Update insert-node-in-a-binary-search-tree.java

--- lint-code/code/insert-node-in-a-binary-search-tree.java
+++ lint-code/code/insert-node-in-a-binary-search-tree.java
@@ -25,3 +25,47 @@ public class Solution {
         return root;
     }
 }
+
+
+//Approach 2: Iterative
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param root: The root of the binary search tree.
+     * @param node: insert this node into the binary search tree
+     * @return: The root of the new binary search tree.
+     */
+    public TreeNode insertNode(TreeNode root, TreeNode node) {
+        // write your code here
+        if(root==null) return node;
+        TreeNode parent = root;
+        while(parent!=null){
+            if(node.val <= parent.val){
+                if(parent.left==null){
+                    parent.left = node;
+                    return root;
+                }
+                parent = parent.left;
+            }else{
+                if(parent.right==null){
+                    parent.right = node;
+                    return root;
+                }
+                parent = parent.right;
+            }
+        }
+        
+        return root;
+    }
+}
+
