87ec7ad5b051524344bdaf1bb68fdbf223edca28
Huangxin <maggiewangsjtu@gmail.com>
2015-02-09 13:13:06 -0500

    Update clone-graph.java

--- lint-code/code/clone-graph.java
+++ lint-code/code/clone-graph.java
@@ -1,3 +1,6 @@
+//Approach 1: DFs
+//Error: Stack overflow
+
 /**
  * Definition for undirected graph.
  * class UndirectedGraphNode {
@@ -39,5 +42,59 @@ public class Solution {
         
         newnode.neighbors = copyNeighbors;
         map.put(newnode.label, newnode);
+    }
+}
+
+
+
+//Approach 2: BFS
+/**
+ * Definition for undirected graph.
+ * class UndirectedGraphNode {
+ *     int label;
+ *     ArrayList<UndirectedGraphNode> neighbors;
+ *     UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+ * };
+ */
+public class Solution {
+    /**
+     * @param node: A undirected graph node
+     * @return: A undirected graph node
+     */
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        // write your code here
+        if(node==null) return null;
+        
+        HashMap<Integer, UndirectedGraphNode> map = new HashMap<Integer, UndirectedGraphNode>();
+        LinkedList<UndirectedGraphNode> queue = new LinkedList<UndirectedGraphNode>();
+        queue.add(node);
+        bfs(queue, map);
+        return map.get(node.label);
+    }
+    
+    public void  bfs(LinkedList<UndirectedGraphNode> queue, HashMap<Integer, UndirectedGraphNode> map){
+        
+        while(!queue.isEmpty())
+        { 
+            UndirectedGraphNode node = queue.removeFirst();
+            UndirectedGraphNode newnode = new UndirectedGraphNode(node.label);
+            if(map.containsKey(node.label)) newnode = map.get(node.label);
+            
+            map.put(node.label, newnode);
+            
+            ArrayList<UndirectedGraphNode> neighbors = node.neighbors;
+            
+            for(int i=0; i<neighbors.size(); i++){
+                int label =  neighbors.get(i).label;
+                if(!map.containsKey(label)){
+                    queue.add(neighbors.get(i));
+                    UndirectedGraphNode newNeighborNode = new UndirectedGraphNode(label);
+                    map.put(label, newNeighborNode);
+                }
+                
+                newnode.neighbors.add(map.get(label));
+            }
+       
+        }
     }
 }
