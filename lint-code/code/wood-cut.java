1ff52a7cc99c713a32f3101e07893933ee214f2e
Huangxin <maggiewangsjtu@gmail.com>
2015-02-11 01:01:39 -0500

    Create wood-cut.java

---
+++ lint-code/code/wood-cut.java
@@ -0,0 +1,39 @@
+public class Solution {
+    /** 
+     *@param L: Given n pieces of wood with length L[i]
+     *@param k: An integer
+     *return: The maximum length of the small pieces.
+     */
+    public int woodCut(int[] L, int k) {
+        // write your code here
+        int len = 1;
+        int maxLen = 0;
+        
+        for(int i=0; i<L.length; i++){
+            if(L[i] > maxLen) maxLen = L[i];
+        }
+        int step = (int)Math.log(maxLen);
+        
+        while(true){
+            if(canCut(L,k, len)){
+                len = Math.min(len+step, maxLen);
+            }
+            else break;
+        }
+        
+        for(int i = len-step+1; i<=len; i++){
+            if(!canCut(L,k,i)) return i-1;
+        }
+        
+        return len;
+    }
+    
+    public boolean canCut(int[] L, int k, int len){
+        int rnt = 0;
+        for(int i=0; i<L.length; i++){
+            rnt += (L[i]/len);
+        }
+        if(rnt>=k) return true;
+        return false;
+    }
+}
