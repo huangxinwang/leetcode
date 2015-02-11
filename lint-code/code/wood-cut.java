430b34f1bf783c62506ff4db65803b7ce217626e
Huangxin <maggiewangsjtu@gmail.com>
2015-02-11 01:59:20 -0500

    Update wood-cut.java

--- lint-code/code/wood-cut.java
+++ lint-code/code/wood-cut.java
@@ -1,3 +1,4 @@
+//Approach 1: using break egg method, the time complexity is not satisfied
 public class Solution {
     /** 
      *@param L: Given n pieces of wood with length L[i]
@@ -38,6 +39,48 @@ public class Solution {
         for(int i=0; i<L.length; i++){
             rnt += (L[i]/len);
             //return when meet requirements, otherwise, may overflow
+            if(rnt>=k) return true;
+        }
+        
+        return false;
+    }
+}
+
+
+//Approach 2: Binary search
+public class Solution {
+    /** 
+     *@param L: Given n pieces of wood with length L[i]
+     *@param k: An integer
+     *return: The maximum length of the small pieces.
+     */
+    public int woodCut(int[] L, int k) {
+        // write your code here
+        if(L.length==0) return 0;
+        int maxLen = 0;
+        
+        for(int i=0; i<L.length; i++){
+            if(L[i] > maxLen) maxLen = L[i];
+        }
+       
+        int left = 1;
+        int right = maxLen;
+        while(left<right){
+            if(canCut(L,k,right)) return right;
+            //if(!canCut(L,k,left)) return left-1;
+            int mid = left + (right-left)/2;
+            if(canCut(L,k,mid)){
+                left = mid+1;
+            }else right = mid-1;
+        }
+        
+        return left-1;
+    }
+    
+    public boolean canCut(int[] L, int k, int len){
+        int rnt = 0;
+        for(int i=0; i<L.length; i++){
+            rnt += (L[i]/len);
             if(rnt>=k) return true;
         }
         
