0da4d23deafa44dd75e24e29ba4cc69711195552
Huangxin <maggiewangsjtu@gmail.com>
2015-02-15 20:23:13 -0500

    Update median-of-two-sorted-array.java

--- lint-code/code/median-of-two-sorted-array.java
+++ lint-code/code/median-of-two-sorted-array.java
@@ -1,3 +1,6 @@
+
+
+
 class Solution {
     /**
      * @param A: An integer array.
@@ -6,39 +9,49 @@ class Solution {
      */
     public double findMedianSortedArrays(int[] A, int[] B) {
         // write your code here
-        int n = A.length-1;
-        int m = B.length-1;
+        int n = A.length;
+        int m = B.length;
         
         if( (n+m)%2==0){
-            return (getMedian(A, 0, n, B, 0,m, (n+m)/2) + getMedian(A,0, n, B, 0,m, (n+m)/2+1))/2.0;
+            double m1 = getMedian(A, 0, n, B, 0, m, (n+m)/2);
+            double m2 = getMedian(A,0, n, B, 0,m, (n+m)/2+1);
+            //System.out.println(m1+" "+m2);
+            return (m1+m2)/2.0;
         }
-        else return getMedian(A, 0, n, B, 0, m, (n+m)/2+1)/2.0;
+        else return getMedian(A, 0, n, B, 0, m, (n+m+1)/2);
     }
     
+
+    //right index is excluded
+    //use length to judge whether exceed k or not
     public double getMedian(int[] A, int leftA, int rightA, int[] B, int leftB, int rightB, int k){
-        
-        if(leftA>rightA) return B[leftB+k-1];
-        if(leftB>rightB) return A[leftA+k-1];
+  
+        if(leftA>=rightA) return B[leftB+k-1];
+        if(leftB>=rightB) return A[leftA+k-1];
         if(k<=1) return A[leftA] <= B[leftB] ? A[leftA] : B[leftB];
         
+        int n = (rightA-leftA);
+        int m = (rightB-leftB);
         
-        int midA = leftA + (rightA - leftA)/2;
-        int midB = leftB + (rightB - leftB)/2;
+        int midA = (rightA + leftA)/2;
+        int midB = (rightB + leftB)/2;
         
-        if(A[midA] <= B[midB]){
-            if(midA-leftA+midB-leftB+1 >=k)
-                return getMedian(A, leftA, rightA, B, leftB, midB-1, k);
-            else  return getMedian(A, midA+1, rightA, B, leftB, rightB, k-(midA-leftA+1));
-             
-        }
-        else{
-            if(midA-leftA+midB-leftB+1 >= k)
-               return getMedian(A, leftA, midA-1, B, leftB, rightB, k);
-                
+        
+        if(n/2+m/2+1 >= k)
+        {
+            if(A[midA] < B[midB])
+                return getMedian(A, leftA, rightA, B, leftB, midB, k);
             else
-               return getMedian(A, leftA, rightA, B, midB+1, rightB,k-(midB-leftB+1));
+                return getMedian(A, leftA, midA, B, leftB, rightB, k);
         }
+        else
+        {
+            if(A[midA] < B[midB])
+                return getMedian(A, midA+1, rightA, B, leftB, rightB, k-n/2-1);
+            else
+                return getMedian(A, leftA, rightA, B, midB+1, rightB, k-m/2-1);
+        }
+        
     }
     
 }
-
