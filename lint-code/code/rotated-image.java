814e2e1e7c5c9fac3b29bdc26c4b78dcf58ba39a
Huangxin <maggiewangsjtu@gmail.com>
2015-02-19 21:51:38 -0500

    Create rotated-image.java

---
+++ lint-code/code/rotated-image.java
@@ -0,0 +1,19 @@
+public class Solution {
+    /**
+     * @param matrix: A list of lists of integers
+     * @return: Void
+     */
+    public void rotate(int[][] matrix) {
+        // write your code here
+        int n = matrix.length;
+        for(int i=0; i<(n+1)/2; i++){
+            for(int j=0; j<n/2; j++){
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[n-1-j][i];
+                matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+                matrix[j][n-1-i] = tmp;
+            }
+        }
+    }
+}
