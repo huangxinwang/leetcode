033c6a8daff55baeca64f709dbb2b5b9e3d5c487
Huangxin <maggiewangsjtu@gmail.com>
2015-02-21 13:17:20 -0500

    Update word-segmentation.java

--- lint-code/code/word-segmentation.java
+++ lint-code/code/word-segmentation.java
@@ -4,7 +4,21 @@ public class Solution {
      * @param dict: A dictionary of words dict
      */
     public boolean wordSegmentation(String s, Set<String> dict) {
-        // write your code here   
+        // write your code here 
+        
+        //special case, required char is not in dict
+        char[] charset = new char[256];
+        for(String word: dict){
+            for(char c : word.toCharArray())
+                charset[c]++;
+        }
+        
+        for(char c: s.toCharArray()){
+            if(charset[c]==0)
+                return false;
+        }
+        
+        //dynamic programming
         boolean[] state = new boolean[s.length()+1];
         state[0] = true;
         
