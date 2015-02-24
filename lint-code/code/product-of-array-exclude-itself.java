cdc9cbe6363c4cd2f52cb91fafbc66ed21678d22
Huangxin <maggiewangsjtu@gmail.com>
2015-02-24 15:12:04 -0500

    Update product-of-array-exclude-itself.java

--- lint-code/code/product-of-array-exclude-itself.java
+++ lint-code/code/product-of-array-exclude-itself.java
@@ -40,3 +40,42 @@ public class Solution {
     }
 }
 
+
+//////////////////////////////////////////////////////////////////////////////
+//Approach 2: Scan from left and right; build up dynamic table; and then scan from right to left
+//Time complexity: O(n)
+//Space complexity: O(n)
+
+public class Solution {
+    /**
+     * @param A: Given an integers array A
+     * @return: A Long array B and B[i]= A[0] * ... * A[i-1] * A[i+1] * ... * A[n-1]
+     */
+    public ArrayList<Long> productExcludeItself(ArrayList<Integer> A) {
+        // write your code
+        ArrayList<Long> rnt = new ArrayList<Long>();
+        if(A.size()<=1) return rnt;
+        
+        //leftPro[i] represents the product of A[0]*A[1]*...*A[i]
+        Long[] leftPro = new Long[A.size()-1];
+        long left = 1;
+        for(int i=0; i<A.size()-1; i++){
+            left = left*A.get(i);
+            leftPro[i] = left;
+        }
+        
+        //calculate the product with the absence of the ith element
+        long right = 1;
+        for(int i=A.size()-1; i>=1; i--){
+            long pro = leftPro[i-1]*right;
+            right = right*A.get(i);
+            rnt.add(0, pro);
+        }
+        
+        rnt.add(0, right);
+        
+        return rnt;
+    }
+}
+
+
