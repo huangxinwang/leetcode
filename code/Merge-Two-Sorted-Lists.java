a0bd0845c718255c7e0a55f09ffce61c3b786da2
Huangxin <maggiewangsjtu@gmail.com>
2015-10-25 15:07:22 -0400

    Update Merge-Two-Sorted-Lists.java

--- code/Merge-Two-Sorted-Lists.java
+++ code/Merge-Two-Sorted-Lists.java
@@ -125,3 +125,39 @@ public class Solution {
         }
     }
 }
+
+
+////////////////////////////////////////////////////////////////////////
+// 2015/10/25
+// Highlight: use a dummyhead and a curr pointer, traverse two lists when neither is null
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(-1);
+        ListNode curr = dummyHead;
+        if(l1==null) return l2;
+        if(l2==null) return l1;
+        
+        while(l1!=null && l2!=null){
+            if(l1.val <=l2.val){
+                curr.next = l1;
+                l1 = l1.next;
+            }else{
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+        
+        if(l1!=null) curr.next = l1;
+        if(l2!=null) curr.next = l2;
+        return dummyHead.next;
+    }
+}
