681a344bc298c6d7f693582036d58f4ea54d831f
Huangxin <maggiewangsjtu@gmail.com>
2015-10-25 16:22:11 -0400

    Update Merge-k-Sorted-Lists.java

--- code/Merge-k-Sorted-Lists.java
+++ code/Merge-k-Sorted-Lists.java
@@ -119,3 +119,50 @@ public class Solution {
     }
     
 }
+
+
+////////////////////////////////////////////////////
+// 2015/10/25
+// Highlight: recursive
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists==null ||lists.length==0) return null;
+        if(lists.length==1) return lists[0];
+        ListNode[] rnt = new ListNode[(lists.length+1)/2];
+        for(int i=0; i<lists.length/2; i++){
+            rnt[i] = mergeTwoList(lists[i], lists[lists.length-1-i]);
+        }
+        if(lists.length%2==1) rnt[lists.length/2] = lists[lists.length/2];
+        return mergeKLists(rnt);
+    }
+    
+    public ListNode mergeTwoList(ListNode l1, ListNode l2){
+        ListNode dummyHead = new ListNode(-1);
+        ListNode curr = dummyHead;
+        if(l1==null) return l2;
+        if(l2==null) return l1;
+        
+        while(l1!=null && l2!=null){
+            if(l1.val <= l2.val){
+                curr.next = l1;
+                l1 = l1.next;
+            }else{
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+        
+        if(l1!=null) curr.next = l1;
+        if(l2!=null) curr.next = l2;
+        return dummyHead.next;
+    }
+}
