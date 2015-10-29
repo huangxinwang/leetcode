fd316d857b1c5fdc7c83838409f6d6aa1a2bec36
Huangxin <maggiewangsjtu@gmail.com>
2015-10-29 02:19:46 -0400

    Update valid_anagram.java

--- code/valid_anagram.java
+++ code/valid_anagram.java
@@ -1,4 +1,10 @@
+// Problem Statement: Valid Anagram
 
+// Given two strings s and t, write a function to determine if t is an anagram of s.
+
+// For example,
+// s = "anagram", t = "nagaram", return true.
+// s = "rat", t = "car", return false.
 
 
 // 2015/10/29
