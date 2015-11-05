2bb49b28c56c25aa0e1be5a69c65b9a8aa6f0768
Huangxin <maggiewangsjtu@gmail.com>
2015-11-05 07:48:47 -0800

    Update Group-Anagrams.java

--- code/Group-Anagrams.java
+++ code/Group-Anagrams.java
@@ -1,6 +1,16 @@
 /**
  * Problem Statement: Anagrams 
  * 
+ * Given an array of strings, group anagrams together.
+
+For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"], 
+Return:
+
+[
+  ["ate", "eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
  */
 
 
