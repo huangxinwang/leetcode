7eb39d51b85e0684635cdff049bff311cb2d36ae
Huangxin <maggiewangsjtu@gmail.com>
2015-11-05 08:33:33 -0800

    Update Pow.java

--- code/Pow.java
+++ code/Pow.java
@@ -61,3 +61,26 @@ public class Solution {
         else return half*half*x;
     }
 }
+
+
+////////////////////////////////////////////////////
+// 2015/11/05
+// Highlight: recursive
+public class Solution {
+    public double myPow(double x, int n) {
+        if(n<0){
+            n = -n;
+            return 1/help(x,n);
+        }
+        return help(x,n);
+    }
+    
+    //recursive! cool
+    public double help(double x, int n){
+        if(n==0) return 1;
+        double half = help(x, n/2);
+        if(n%2==0)
+            return half*half;
+        else return half*half*x;
+    }
+}
