ff2023ee3e8adbb01f7a2260654cc67a87f15efe
Huangxin <maggiewangsjtu@gmail.com>
2015-11-05 20:37:42 -0800

    Update Permutation-Sequence.java

--- code/Permutation-Sequence.java
+++ code/Permutation-Sequence.java
@@ -33,3 +33,44 @@ public class Solution {
         return fact;
     }
 }
+
+
+/////////////////////////////////////////////
+// 2015/11/05
+// Comment: cool
+// Highlight: combination number and factorial!
+public class Solution {
+    public String getPermutation(int n, int k) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(int i=1; i<=n; i++){
+            list.add(i);
+        }
+        
+        return help(list, k-1);
+    }
+    
+    public String help(ArrayList<Integer> list, int k){
+        int n = list.size();
+        if(n==1) return list.get(0)+"";
+        
+        int sum = factorial(n-1);
+        
+        //result corresponding to the number in the list
+        //The factorial of (n-1) number indicates the number of combinations of (n-1) numbers
+        //Therefore, we can reduce the number of k by mode (factorial of n-1)
+        int result = k/sum;
+        k = k%sum;
+        
+        int curr = list.get(result);
+        list.remove(result);
+        
+        return curr + help(list, k);
+        
+    }
+    
+    public int factorial(int n){
+        if(n==1) return 1;
+        int rnt = n;
+        return rnt*factorial(n-1);
+    }
+}
