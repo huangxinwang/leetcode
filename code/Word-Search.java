a632febbe63d07ad931f387f436f38adce21ea1f
Huangxin <maggiewangsjtu@gmail.com>
2015-11-07 18:26:11 -0800

    Update Word-Search.java

--- code/Word-Search.java
+++ code/Word-Search.java
@@ -141,3 +141,48 @@ public class Solution {
         return state[x][y]==false;
     }
 }
+
+
+//////////////////////////////////////////////////////////////
+// 2015/11/07
+// Highlight: start at each position, and use dfs to see if any match exist
+public class Solution {
+    public boolean exist(char[][] board, String word) {
+        int m = board.length;
+        int n = board[0].length;
+        
+        
+        //try to start at each position
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(dfs(board, word, i, j, 0)) return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    //dfs to find next char in target string
+    public boolean dfs(char[][] board, String word, int i, int j, int k){
+        int m = board.length;
+        int n = board[0].length;
+        
+        if(i<0 || i>=m || j<0 || j>=n) return false;
+        
+        if(word.charAt(k)==board[i][j]){
+            char tmp = board[i][j];
+            
+            if(k==word.length()-1) return true;
+            
+            //use a char to indicate this box has been usd
+            board[i][j] = '#';
+            
+            if( dfs(board, word, i-1, j, k+1) ||
+                dfs(board, word, i, j+1, k+1) ||
+                dfs(board, word, i+1, j, k+1) ||
+                dfs(board, word, i, j-1, k+1) ) return true;
+            board[i][j] = tmp;
+        }
+        return false;
+    }
+}
