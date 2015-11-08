4182f8cdaa3cf2bab4512ee378ef94a01b6e2098
Huangxin <maggiewangsjtu@gmail.com>
2015-11-08 09:57:24 -0800

    Update Merge-Sorted-Array.java

--- code/Merge-Sorted-Array.java
+++ code/Merge-Sorted-Array.java
@@ -57,3 +57,27 @@ public class Solution {
         }
     }
 }
+
+
+/////////////////////////////////////////////////
+// 2015/11/08
+// Highlight: write from end to start in order not to override
+public class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+        while(m>0 && n>0){
+            if(nums1[m-1]>nums2[n-1]){
+                nums1[m+n-1] = nums1[m-1];
+                m--;
+            }else{
+                nums1[m+n-1] = nums2[n-1];
+                n--;
+            }
+        }
+        
+        while(n>0){
+            nums1[m+n-1] = nums2[n-1];
+            n--;
+        }
+    }
+}
