dec968817793d3bacdd8a700397d6207ebfeaa8a
Huangxin <maggiewangsjtu@gmail.com>
2015-11-09 11:00:30 -0500

    Update Balanced-Binary-Tree.java

--- code/Balanced-Binary-Tree.java
+++ code/Balanced-Binary-Tree.java
@@ -95,3 +95,36 @@ public class Solution {
         else return leftVal>rightVal ? leftVal+1 : rightVal+1;
     }
 }
+
+
+///////////////////////////////////////////////////
+// 2015/11/09
+// Highlight: recursive
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        int val = help(root);
+        if(val==-1) return false;
+        return true;
+    }
+    
+    public int help(TreeNode root){
+        if(root==null) return 0;
+        
+        int left = help(root.left);
+        int right = help(root.right);
+        
+        if(left==-1 || right==-1) return -1;
+        if(left>right+1 || left<right-1) return -1;
+        //note need to add 1
+        else return left>right ? left+1 : right+1;
+    }
+}
