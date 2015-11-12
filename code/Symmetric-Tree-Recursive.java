17209fb425fef8296a9037bbb18c1bc0e35ca249
Huangxin <maggiewangsjtu@gmail.com>
2015-11-11 21:31:22 -0500

    Update Symmetric-Tree-Recursive.java

--- code/Symmetric-Tree-Recursive.java
+++ code/Symmetric-Tree-Recursive.java
@@ -98,3 +98,33 @@ public class Solution {
         
     }
 }
+
+
+/////////////////////////////////////////////////////////
+// 2015/11/11
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null) return true;
+        return isSyn(root.left, root.right);
+    }
+    
+    public boolean isSyn(TreeNode left, TreeNode right){
+        if(left==null && right==null) return true;
+        if(left==null) return right==null;
+        if(right==null) return left==null;
+        
+        if(left.val!=right.val) return false;
+        if(!isSyn(left.left, right.right)) return false;
+        if(!isSyn(left.right, right.left)) return false;
+        return true;
+    }
+}
