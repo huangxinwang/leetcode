6d457268389e29d49131dc42c0c162c30f1b9903
Huangxin <maggiewangsjtu@gmail.com>
2015-11-11 23:10:53 -0500

    Update Path-Sum.java

--- code/Path-Sum.java
+++ code/Path-Sum.java
@@ -68,3 +68,30 @@ public class Solution {
         return false;
     }
 }
+
+
+/////////////////////////////////////////////////////////
+// 2015/11/11
+// Highlight: recursive
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root==null) return false;
+        
+        //case when condition satisfied
+        if(root.left==null && root.right==null && sum-root.val==0) return true;
+        
+        if(hasPathSum(root.left, sum-root.val)) return true;
+        if(hasPathSum(root.right, sum - root.val)) return true;
+        
+        return false;
+    }
+}
