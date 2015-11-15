9ccc0c76d72847a6018e7e4c845f18e6f1ea42fe
Huangxin <maggiewangsjtu@gmail.com>
2015-11-14 20:57:08 -0500

    Update Excel-Sheet-Column-Number.java

--- code/Excel-Sheet-Column-Number.java
+++ code/Excel-Sheet-Column-Number.java
@@ -15,3 +15,43 @@ public class Solution {
         return rnt;
     }
 }
+
+////////////////////////////////////////
+// 2015/11/14
+//Highlight: traverse from right to left seems more complicated
+public class Solution {
+    public int titleToNumber(String s) {
+        
+        int rnt = 0;
+        int carry = 0;
+        //unit
+        int unit = 1;
+        
+        //add from right to left
+        for(int i=s.length()-1; i>=0; i--){
+            char c = s.charAt(i);
+            int sum = c - 'A'+1+carry;
+            rnt = rnt + (sum%26)*unit;
+            carry = sum/26;
+            unit *= 26;
+        }
+        if(carry!=0){
+            rnt = rnt + carry*unit;
+        }
+        return rnt;
+    }
+}
+
+////////////////////////////////////////////
+// 2015/11/14
+// Highlight: try traverse from left to right
+public class Solution {
+    public int titleToNumber(String s) {
+        int rnt = 0;
+        for(int i=0; i<s.length(); i++){
+            int val = s.charAt(i) - 'A' + 1;
+            rnt = rnt*26 + val;
+        }
+        return rnt;
+    }
+}
