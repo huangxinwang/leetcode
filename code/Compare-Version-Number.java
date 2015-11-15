d2acb5aa6ca1378c6ea940651fb6840eb6ffb7c0
Huangxin <maggiewangsjtu@gmail.com>
2015-11-14 21:58:26 -0500

    Update Compare-Version-Number.java

--- code/Compare-Version-Number.java
+++ code/Compare-Version-Number.java
@@ -29,3 +29,27 @@ public class Solution {
         
     }
 }
+
+
+///////////////////////////////////////////////////////////////////////
+//2015/11/14
+// Highlight: string split and compare
+public class Solution {
+    public int compareVersion(String version1, String version2) {
+        
+        //split strings
+        String[] str1 = version1.split("\\.");
+        String[] str2 = version2.split("\\.");
+        
+        //compare each string, if not exist, use 0
+        int i=0;
+        for(; i<Math.max(str1.length, str2.length); i++){
+            int val1 = i<str1.length? Integer.parseInt(str1[i]) : 0;
+            int val2 = i<str2.length? Integer.parseInt(str2[i]) : 0;
+            if(val1>val2) return 1;
+            if(val1<val2) return -1;
+        }
+
+        return 0;
+    }
+}
