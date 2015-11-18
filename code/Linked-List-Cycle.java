2712e6547bb3154d0b885f7c0cc3160197a5c53d
Huangxin <maggiewangsjtu@gmail.com>
2015-11-18 16:52:38 -0500

    Update Linked-List-Cycle.java

--- code/Linked-List-Cycle.java
+++ code/Linked-List-Cycle.java
@@ -82,3 +82,33 @@ public class Solution {
         return false;
     }
 }
+
+
+//////////////////////////////////////////////////////
+// 2015/11/18
+// slow and fast pointer
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head==null) return false;
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast==slow) return true;
+        }
+        
+        return false;
+    }
+}
