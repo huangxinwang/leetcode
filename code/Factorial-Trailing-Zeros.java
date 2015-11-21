40ee4f853935af178da9110a85fe8d6d0df45e63
Huangxin <maggiewangsjtu@gmail.com>
2015-11-20 20:37:34 -0500

    Update Factorial-Trailing-Zeros.java

--- code/Factorial-Trailing-Zeros.java
+++ code/Factorial-Trailing-Zeros.java
@@ -12,3 +12,15 @@ ublic class Solution {
         else return n/5+trailingZeroes(n/5);
     }
 }
+
+/////////////////////////////////////
+// 2015/11/20
+// Highlight: logic
+public class Solution {
+    public int trailingZeroes(int n) {
+        if(n==0) return 0;
+        //count number of 5 in numbers in 1...n
+        //Note: for example 125 = 5*5*5 contains 3 of 5
+        return n/5 + trailingZeroes(n/5);
+    }
+}
