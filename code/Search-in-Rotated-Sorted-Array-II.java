26f7d676c5d7fd2901c4764b7458640b4b5b204f
Huangxin <maggiewangsjtu@gmail.com>
2015-11-28 23:39:41 -0500

    Update Search-in-Rotated-Sorted-Array-II.java

--- code/Search-in-Rotated-Sorted-Array-II.java
+++ code/Search-in-Rotated-Sorted-Array-II.java
@@ -9,101 +9,32 @@
  * 
  */
 
-public class Solution {
-    public boolean search(int[] A, int target) {
-        //special case
-        if(A.length==0) return false;
-        
-        int left = 0;
-        int right = A.length-1;
-        return searchRotate(A, left, right, target);
-    }
-    
-    public boolean searchRotate(int[] A, int left, int right, int target){
-        int mid = (left+right)/2;
-        //terminate conditions
-        if(mid>=right && A[mid]!=target) return false;
-        if(A[mid] == target) return true;
-        
-        //Case1: consider instance [1 1 1 3 1], both side need rotate search
-        if(A[left]==A[mid] || A[mid]==A[right]) return searchRotate(A, mid+1, right, target) || searchRotate(A, left, mid, target);
-        //Case2:
-        if(A[left]<A[mid]){
-            return binarySearch(A, left, mid, target) || searchRotate(A, mid+1, right, target);
-        }
-        //Case3:
-        else{
-            return searchRotate(A, left, mid, target) || binarySearch(A, mid+1, right, target);
-        }
-    }
-    
-    public boolean binarySearch(int[] A, int left, int right, int target){
-        int mid = (left+right)/2;
-        //terminate condition
-        if(mid>=right && A[mid]!=target) return false;
-        if(A[mid]==target) return true;
-        
-        //two cases
-        if(A[mid]>target) return binarySearch(A, left, mid, target);
-        else return binarySearch(A, mid+1, right, target);
-    }
-}
-
-
-/////////////////////////////////////////////////////////
-//Round 2: 12/19/2014
-public class Solution {
-    public boolean search(int[] A, int target) {
-        
-        return subsearch(A, target, 0, A.length-1);
-    }
-    
-    public boolean subsearch(int[] A, int target, int left, int right){
-        if(left>right) return false;
-        if(left==right) return target == A[left];
-        
-        int mid = (left+right)/2;
-        if(A[mid]==target) return true;
-        
-        //search one side
-        if(A[left]<A[mid] && A[mid] < A[right]){
-            if(target<A[mid])
-                return subsearch(A, target, left, mid-1);
-            else 
-                return subsearch(A, target, mid+1, right);
-        }
-        //search both side
-        else{
-            if(subsearch(A,target, left, mid-1)) return true;
-            else return subsearch(A, target, mid+1, right);
-        }
-    }
-    
-    
-}
-
-
-/////////////////////////////////////////////////////////////
-// 2015/11/08
-// Comment: need to discuss to reduce time complexity to O(lgn), currently, the time complexity is O(n)
+////////////////////////////////////////////////////
+// 2015/11/28
+// Highlight: if leftval == midval, then we can only advance the leftpointer by 1
+// Ref: http://blog.csdn.net/linhuanmars/article/details/20588511
 public class Solution {
     public boolean search(int[] nums, int target) {
-        return bs(nums, 0, nums.length-1, target);
-        
+        return bs(nums, target, 0, nums.length-1);
     }
     
-    public boolean bs(int[] nums, int left, int right, int target){
-        if(left>=right){
-            if(left<nums.length && nums[left]==target) return true;
-             if(right>0 && nums[right]==target) return true;
-            return false;
-        }
-        
-        int mid = (left+right)/2;
+    public boolean bs(int[] nums, int target, int left, int right){
+        if(left>right) return false;
+        int mid = left + (right-left)/2;
         
         if(nums[mid]==target) return true;
-        if(bs(nums,left,mid-1,target)) return true;
-        if(bs(nums,mid+1,right, target)) return true;
-        return false;
+        
+        if(nums[left]<nums[mid]){
+            if(nums[left]<=target && target<nums[mid])
+                return bs(nums,target, left, mid-1);
+            else return bs(nums, target, mid+1, right);
+        }
+        else if(nums[left] > nums[mid]){
+            if(nums[mid]<target && target<=nums[right])
+                return bs(nums, target, mid+1, right);
+            else return bs(nums, target, left, mid-1);
+        }else{
+            return bs(nums, target, left+1, right);
+        }
     }
 }
