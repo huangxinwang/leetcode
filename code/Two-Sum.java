44a82a574c4e7353a0ef23f1cf8e7395c8e0bfc7
Huangxin <maggiewangsjtu@gmail.com>
2015-11-30 11:27:20 -0500

    Update Two-Sum.java

--- code/Two-Sum.java
+++ code/Two-Sum.java
@@ -3,73 +3,28 @@
  * 
  * 
  */
+
+//////////////////////////////////////
+// 2015/11/30
+// Highlight: hashmap, check if (target-curr) is in the map
+// Time: O(n)
 public class Solution {
-    public int[] twoSum(int[] numbers, int target) {
-        
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for(int i=0; i<numbers.length; i++){
-            map.put(numbers[i],i);
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i=0; i<nums.length; i++){
+            map.put(nums[i],i+1);
         }
         
-        int[] rnt = new int[2];
-        
-        //if target - numbers[i] is in hashmap, then found
-        //note number[i] could equal to remain
-        for(int i=0; i<numbers.length; i++){
-            int remain = target - numbers[i];
-            if(map.containsKey(remain) && map.get(remain)!=i){
+        for(int i=0; i<nums.length; i++){
+            int remain = target - nums[i];
+            if(map.containsKey(remain) && map.get(remain)!=i+1){
+                int[] rnt = new int[2];
                 rnt[0] = i+1;
-                rnt[1] = map.get(remain)+1;
+                rnt[1] = map.get(remain);
                 return rnt;
             }
         }
         
-        return rnt;
-    }
-}
-
-
-//Naive approach 2015-10-03
-//Time complexity O(n^2)
-public class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int[] rnt = new int[2];
-        for(int i=0; i<nums.length; i++){
-            int remain = target - nums[i];
-            for(int j=i+1; j<nums.length; j++){
-                if(nums[j]==remain){
-                    rnt[0] = i+1;
-                    rnt[1] = j+1;
-                    return rnt;
-                }
-            }
-        }
-        return rnt;
-        
-    }
-}
-
-
-//HashMap 
-//Time complexity: O(n)
-public class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> maps = new HashMap<Integer, Integer>();
-        for(int i=0; i<nums.length; i++){
-            maps.put(nums[i],i+1);
-        }
-        
-        int[] rnt = new int[2];
-        for(int i=0; i<nums.length; i++){
-            int remain = target - nums[i];
-            if(maps.containsKey(remain) && maps.get(remain)!=i+1){
-                rnt[0] = i+1;
-                rnt[1] = maps.get(remain);
-                break;
-            }
-        }
-        
-        return rnt;
-        
+        return null;
     }
 }
