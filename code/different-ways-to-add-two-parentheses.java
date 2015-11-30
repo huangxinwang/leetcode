fa952fe2cdf52652cda1df43d524e5876c21f463
Huangxin <maggiewangsjtu@gmail.com>
2015-11-30 12:42:06 -0500

    Update different-ways-to-add-two-parentheses.java

--- code/different-ways-to-add-two-parentheses.java
+++ code/different-ways-to-add-two-parentheses.java
@@ -40,3 +40,49 @@ public class Solution {
         return rnt;
     }
 }
+
+
+////////////////////////////////////////////////////////
+// 2015/11/30
+// Highlight: use a hashmap to keep track of the value list of a given string
+public class Solution {
+    public List<Integer> diffWaysToCompute(String input) {
+        Map<String, List<Integer>> map = new HashMap<String, List<Integer>>();
+        return help(input, map);
+    }
+    
+    //use a hashmap to maintain the results of a given string to avoid repeated calculation
+    public List<Integer> help(String input, Map<String, List<Integer>> map){
+        List<Integer> rnt = new ArrayList<Integer>();
+        for(int i=0; i<input.length(); i++){
+            char c = input.charAt(i);
+            if(c!='+' && c!='-' && c!='*') continue;
+            String left = input.substring(0,i);
+            String right = input.substring(i+1, input.length());
+            List<Integer> list1 = map.containsKey(left)? map.get(left): help(left, map);
+            List<Integer> list2 = map.containsKey(right)? map.get(right): help(right, map);
+            
+            for(Integer int1 : list1){
+                for(Integer int2 : list2){
+                    if(c=='+'){
+                        rnt.add(int1+int2);
+                    }
+                    else if(c=='-'){
+                        rnt.add(int1-int2);
+                    }
+                    else{
+                        rnt.add(int1*int2);
+                    }
+                }
+            }
+            
+            
+        }
+        
+        if(rnt.size()==0){
+                rnt.add(Integer.parseInt(input));
+        }
+        map.put(input, rnt);
+        return rnt;
+    }
+}
