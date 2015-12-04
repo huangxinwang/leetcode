3c48bbb89b038d25d34519d416ab517b4175bdd6
Huangxin <maggiewangsjtu@gmail.com>
2015-12-04 01:14:21 -0500

    Create bounded-blocking-queue.java

---
+++ link/file/bounded-blocking-queue.java
@@ -0,0 +1,6 @@
+/**
+* bounded blocking queue
+* Extension: implement multiplewriter()，if one writer want to write more than 1 element，and there is no space available，the writer need to be there wait for the reader，and mean while block other writers.
+*
+*
+*/ 
