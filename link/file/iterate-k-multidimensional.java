1d9f0a2f86223f492d316788915e2f1473edae6a
Huangxin <maggiewangsjtu@gmail.com>
2015-12-04 21:09:54 -0500

    Update and rename iterate-multidimensional.java to iterate-k-multidimensional.java

--- link/file/iterate-k-multidimensional.java
+++ link/file/iterate-k-multidimensional.java
@@ -1,7 +1,10 @@
 // Iterating over Multidimensional Arrays: Use for-each style for on a two-dimensional array
 
+//two dimension
 for(int x[] : nums){
   for(int y : x){
     //do something
   }
 }
+
+//what if k-dimension?
