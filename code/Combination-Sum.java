d5357e93a2c85bcf6eb674d19706322b3ee17468
Huangxin <maggiewangsjtu@gmail.com>
2015-12-07 12:15:41 -0500

    Update Combination-Sum.java

--- code/Combination-Sum.java
+++ code/Combination-Sum.java
@@ -12,86 +12,42 @@
  */
 
 
-public class Solution {
-    public ArrayList<ArrayList<Integer>> combinationSum(int[] candidates, int target) {
-        
-        //declare parameters
-        ArrayList<ArrayList<Integer>> rnt = new ArrayList<ArrayList<Integer>>();
-        int sum = 0;
-        int currIndex = 0;
-        ArrayList<Integer> tmp = new ArrayList<Integer>();
-        
-        //sort array
-        Arrays.sort(candidates);
-                
-        //dfs find all solutions      
-        dfs(currIndex, sum, target, candidates, tmp, rnt);
-        
-        return rnt;
-    }
-    
-    public void dfs(int currIndex, int sum, int target, int[] candidates, ArrayList<Integer> tmp, ArrayList<ArrayList<Integer>> rnt){
-        
-        //terminate condition
-        if(sum>target)  return;
-        if(sum==target){
-            rnt.add(new ArrayList<Integer>(tmp));
-            return;
-        }
-        
-        //add current number
-        if(currIndex<candidates.length){
-            int newSum = sum + candidates[currIndex];
-            ArrayList<Integer> newTmp = new ArrayList<Integer>(tmp);
-            newTmp.add(candidates[currIndex]);
-            dfs(currIndex, newSum, target, candidates, newTmp, rnt);
-        }
-        
-        //jump to next number
-        if(currIndex<candidates.length-1){
-            dfs(currIndex+1, sum, target, candidates, tmp, rnt);
-        }
-    }
-}
+//////////////////////////////////////////////////////
+// 2015/12/07
+// Highlight: DFS
 
-
-
-//////////////////////////////////////////////////////////
-// 2015/11/03
-// Highlight: dfs
 public class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        Arrays.sort(candidates);
         List<List<Integer>> rnt = new ArrayList<List<Integer>>();
-        ArrayList<Integer> curr = new ArrayList<Integer>();
+        List<Integer> curr = new ArrayList<Integer>();
+        Arrays.sort(candidates);
         int sum = 0;
-        int depth = 0;
-        dfs(candidates, target, sum, rnt, curr, depth);
+        int idx = 0;
+        dfs(rnt, curr, candidates, target, idx, sum);
         return rnt;
     }
     
-    public void dfs(int[] candidates, int target, int sum, List<List<Integer>> rnt, ArrayList<Integer> curr, int depth){
+    public void dfs(List<List<Integer>> rnt, List<Integer> curr, int[] candidates, int target, int idx, int sum){
+        
+        //terminate cases
         if(sum==target){
-            ArrayList<Integer> newLsit = new ArrayList<Integer>(curr);
-            rnt.add(newLsit);
-            return;
-        }
-        if(depth>=candidates.length || sum>target){
+            rnt.add(new ArrayList<Integer>(curr));
             return;
         }
         
-        //add current value at index depth
-        curr.add(candidates[depth]);
-        dfs(candidates, target, sum+candidates[depth], rnt, curr, depth);
+        if(sum>target) return;
+        if(idx>=candidates.length) return;
+        
+        //add current number
+        curr.add(candidates[idx]);
+        dfs(rnt, curr, candidates, target, idx, sum+candidates[idx]);
         curr.remove(curr.size()-1);
         
-        //not add current value at depth
-        //skip same value
-        int lastVal = candidates[depth];
-        while(depth<candidates.length-1 && candidates[depth+1]==lastVal){
-           depth++;
+        //jump to next number, skip repeated ones
+        while(idx<candidates.length-1 && candidates[idx+1] == candidates[idx]){
+            idx++;
         }
-        depth++;
-        dfs(candidates, target, sum, rnt, curr, depth);
+        if(idx<candidates.length-1)
+            dfs(rnt, curr, candidates, target, idx+1, sum);
     }
 }
