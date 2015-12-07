3c43a703c92bfd9cd114d1c2bbbcf95f048c3e39
Huangxin <maggiewangsjtu@gmail.com>
2015-12-06 23:06:33 -0500

    Create reader_writer_R2.java

---
+++ link/file/reader_writer_R2.java
@@ -0,0 +1,38 @@
+// R2: reader has higher priority, when it is not lead reader; lead reader will wait for all earlier writer to finish
+
+int activeReader = 0;
+binarySemaphore mutex = new binarySemaphore(1);
+binarySemaphore writer_reader_q = new binarySemaphore(1); //initially, it can be use
+
+public class Reader(){
+  
+  public void beginRead(){
+    mutex.P();
+    activeReader++;
+    if(activeReader==1){
+      writer_reader_q.P();
+    }
+    mutex.V();
+  }
+  
+  public abstract void read();
+  
+  public void endRead(){
+    mutex.P();
+    activeReader--;
+    if(activeReader==0){
+      writer_reader_q.V();
+    }
+    mutex.V();
+  }
+}
+
+public class Writer(){
+  public void beginWrite(){
+    writer_reader_q.P();
+  }
+  
+  public void endWrite(){
+    writer_reader_q.V();
+  }
+}
