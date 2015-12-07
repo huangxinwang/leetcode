797604005c1eaf2c42f0ce5f6150ed420f11d4a9
Huangxin <maggiewangsjtu@gmail.com>
2015-12-07 13:25:02 -0500

    Update Pow.java

--- code/Pow.java
+++ code/Pow.java
@@ -3,84 +3,27 @@
  * Implement pow(x, n).
  */
 
-public class Solution {
-    
-    public double pow(double x, int n) {
-        
-        if(n==0) return 1;
-        
-        //make n non-negtive
-        if(n<0){
-            //make -n not overflow
-            if(n==-2147483648){   
-                n = -(n+1);
-                return 1/(x*subPow(x,n,1));
-             }
-             else{
-                 n = -n;
-                 return 1/subPow(x,n,1);
-             }
-        }
-        
-        return subPow(x,n,1);
-    }
-    
-    public double subPow(double x, int n, int currSum)
-    {
-        if(n==1) return x*currSum;
-        
-        int half = n/2;
-        int left = n%2;
-        
-        double leftSum = 1;
-        for(int i=0; i<left; i++)
-            leftSum *= x;
-            
-        double halfSum = subPow(x,half,1);
-        return halfSum*halfSum*leftSum*currSum;
-    }
-}
+////////////////////////////////
+// 2015/12/07
+// Highlight: recursion
+// Time: O(logn)
 
-//////////////////////////////////////////////////////
-//Round 2: 12/30/2014
-public class Solution {
-    public double pow(double x, int n) {
-        
-        if(x==0) return 0;
-        
-        if(n<0)
-            return subPow(1/x, n);
-        else return subPow(x,n);
-    }
-    
-    //divide and conque
-    public double subPow(double x, int n){
-        if(n==0) return 1;
-        double half = subPow(x,n/2);
-        if(n%2==0) return half*half;
-        else return half*half*x;
-    }
-}
-
-
-////////////////////////////////////////////////////
-// 2015/11/05
-// Highlight: recursive
 public class Solution {
     public double myPow(double x, int n) {
-        if(n<0){
-            n = -n;
-            return 1/help(x,n);
+        if(x==0) return 0;
+        //convert negative to positive
+        if(n<0) {
+            if(n==Integer.MIN_VALUE) return 1/myPow(x,Integer.MAX_VALUE)/x;
+            else return 1/myPow(x,-n);
         }
-        return help(x,n);
-    }
-    
-    //recursive! cool
-    public double help(double x, int n){
+        
+        //terminate case
         if(n==0) return 1;
-        double half = help(x, n/2);
-        if(n%2==0)
-            return half*half;
+        if(n==1) return x;
+        
+        //recursive
+        double half = myPow(x, n/2);
+        if(n%2==0) return half*half;
         else return half*half*x;
     }
 }
