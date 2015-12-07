9f5fcef053907dc26c3e998bbc4a3ca8876b9489
Huangxin <maggiewangsjtu@gmail.com>
2015-12-07 15:30:59 -0500

    Update triangle.java

--- link/file/triangle.java
+++ link/file/triangle.java
@@ -5,8 +5,8 @@ public int count(int[] arr){
   //keep the first and second point, search the possible third points
   for(int i=0; i<arr.length-2; i++){
     int k = i+2;
-    for(int j=i+1; j<arr.length-1; j++){
-      while(arr[i]+arr[j]>arr[k])
+    for(int j=i+1; j<arr.length; j++){
+      while(k<n && arr[i]+arr[j]>arr[k])
         k++;
       
       sum += (k-j-1);
