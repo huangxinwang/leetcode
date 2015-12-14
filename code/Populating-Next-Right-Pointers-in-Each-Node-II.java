f03a1e57136ed1fe58ef42232858db7388b75d95
Huangxin <maggiewangsjtu@gmail.com>
2015-12-13 21:10:16 -0500

    Update Populating-Next-Right-Pointers-in-Each-Node-II.java

--- code/Populating-Next-Right-Pointers-in-Each-Node-II.java
+++ code/Populating-Next-Right-Pointers-in-Each-Node-II.java
@@ -12,59 +12,40 @@
  *     TreeLinkNode(int x) { val = x; }
  * }
  */
+/**
+ * Definition for binary tree with next pointer.
+ * public class TreeLinkNode {
+ *     int val;
+ *     TreeLinkNode left, right, next;
+ *     TreeLinkNode(int x) { val = x; }
+ * }
+ */
 public class Solution {
-    
     public void connect(TreeLinkNode root) {
-        
         //special case
         if(root==null) return;
         
-        //use curr and next to keep track of neighour layers
-        ArrayList<TreeLinkNode> curr = new ArrayList<TreeLinkNode>();
-        ArrayList<TreeLinkNode> next = new ArrayList<TreeLinkNode>();
+        //BFS traverse
+        List<TreeLinkNode> curr = new ArrayList<TreeLinkNode>();
+        List<TreeLinkNode> next = new ArrayList<TreeLinkNode>();
         curr.add(root);
-        
-        //BFS traversal
-        while(curr.size()!=0){
-            
-            for(int i=0; i<curr.size();i++){
-                TreeLinkNode currNode = curr.get(i);
-                if(currNode.left!=null) next.add(currNode.left);
-                if(currNode.right!=null) next.add(currNode.right);
-                if(i!=curr.size()-1) currNode.next = curr.get(i+1);
-                else currNode.next = null;
+        while(curr.size()>0){
+            for(int i=0; i<curr.size(); i++){
+                TreeLinkNode node = curr.get(i);
+                if(node.left!=null){
+                    next.add(node.left);
+                }
+                if(node.right!=null){
+                    next.add(node.right);
+                }
+                if(i!=curr.size()-1){
+                    node.next = curr.get(i+1);
+                }else{
+                    node.next = null;
+                }
             }
-            
-            //update curr and next layer
             curr = new ArrayList<TreeLinkNode>(next);
             next = new ArrayList<TreeLinkNode>();
         }
-        
     }
 }
-
-
-/////////////////////////////////////////////////////////////////////////////
-//Round 2: 12/10/2014
-public void connect(TreeLinkNode root) {
-        
-        //use BFS
-        //two layers to keep track of current and next
-        ArrayList<TreeLinkNode> curr = new ArrayList<TreeLinkNode>();
-        ArrayList<TreeLinkNode> next = new ArrayList<TreeLinkNode>();
-        if(root==null) return;
-        
-        curr.add(root);
-        while(curr.size()!=0){
-            for(int i=0; i<=curr.size()-1; i++){
-                TreeLinkNode currNode = curr.get(i);
-                if(i<curr.size()-1)
-                    currNode.next = curr.get(i+1);
-                if(currNode.left!=null) next.add(currNode.left);
-                if(currNode.right!=null) next.add(currNode.right);;
-            }
-            curr.get(curr.size()-1).next = null;
-            curr = new ArrayList<TreeLinkNode>(next);
-            next = new ArrayList<TreeLinkNode>();
-        }
-    }
