854e53b64242337b5493ea736770eff18e732251
Huangxin <maggiewangsjtu@gmail.com>
2015-12-15 01:30:56 -0500

    Update longest-increasing-subsequence.java

--- code/longest-increasing-subsequence.java
+++ code/longest-increasing-subsequence.java
@@ -29,3 +29,40 @@ public class Solution {
         return max;
     }
 }
+
+
+////////////////////////////////////////////////////
+// 2015/12/15
+// Highlight: unlike dynamic programming, in the inner loop of j, use binary search
+// The main idea is to find a position to insert current value
+// Ref: http://www.geeksforgeeks.org/longest-monotonically-increasing-subsequence-size-n-log-n/
+
+public class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if(nums==null || nums.length==0) return 0;
+        int len = 1;
+        int[] tails = new int[nums.length];
+        tails[0] = nums[0];
+        
+        for(int i=1; i<nums.length; i++){
+            //Case 1: replace minimum with current value
+            if(nums[i]<tails[0]) tails[0] = nums[i];
+            //Case 2: append current value
+            else if(nums[i]>tails[len-1]){
+                tails[len++] = nums[i];
+            }
+            //Case 3: find idx to insert current value
+            else tails[findIdx(tails, 0,len, nums[i])] = nums[i];
+        }
+        return len;
+    }
+    
+    //find idx to insert value target
+    public int findIdx(int[] nums, int left, int right, int target){
+        if(right-left<1) return left;
+        int mid = left + (right-left)/2;
+        if(nums[mid]<target){
+            return findIdx(nums, mid+1, right, target);
+        }else return findIdx(nums, left, mid, target);
+    }
+}
