02d3fb0e922f1f8b161ea099052ecac3b4c14311
Huangxin <maggiewangsjtu@gmail.com>
2015-12-16 11:58:05 -0500

    Update combination-sum-II.java

--- code/combination-sum-II.java
+++ code/combination-sum-II.java
@@ -11,48 +11,43 @@
 
 
 ////////////////////////////////////////////////////////
-// 2015/12/07 
+// 2015/12/16
 // Highlight: DFS
 // Time: O(2^n)
 
-public class Solution {
+ 
+ public class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> rnt = new ArrayList<List<Integer>>();
         Arrays.sort(candidates);
-        boolean[] used = new boolean[candidates.length];
+        List<List<Integer>> rnt = new ArrayList<List<Integer>>();
         List<Integer> curr = new ArrayList<Integer>();
+        boolean[] used = new boolean[candidates.length];
         int idx = 0;
         int sum = 0;
-        dfs(rnt, curr, candidates, used, idx, sum, target);
+        dfs(rnt, curr, candidates, used, target, sum, idx);
         return rnt;
+        
     }
     
-    public void dfs(List<List<Integer>> rnt, List<Integer> curr, int[] candidates, boolean[] used, int idx, int sum, int target){
-        
-        //terminate cases
+    public void dfs(List<List<Integer>> rnt, List<Integer> curr, int[] candidates, boolean[] used, int target, int sum, int idx){
         if(sum==target){
             rnt.add(new ArrayList<Integer>(curr));
             return;
         }
-        if(sum>target) return;
         if(idx>=candidates.length) return;
+        if(sum>target) return;
         
-       
-            
-        //skip current due to repeated value
-        if(idx!=0 && candidates[idx-1]==candidates[idx] && used[idx-1]==false)
-            dfs(rnt, curr, candidates, used, idx+1, sum, target);
-        else{
-            //add current
-            curr.add(candidates[idx]);
-            used[idx] = true;
-            dfs(rnt, curr, candidates, used, idx+1, sum+candidates[idx], target);
-            used[idx] = false;
-            curr.remove(curr.size()-1);
-           
-            //jump to next
-            dfs(rnt, curr, candidates, used, idx+1, sum, target);
-        }
+        //not use current number
+        dfs(rnt, curr, candidates, used, target, sum, idx+1);
+        
+        //use current number
+        if(idx!=0 && candidates[idx-1]==candidates[idx] && used[idx-1] ==false) return;
+        used[idx] = true;
+        curr.add(candidates[idx]);
+        dfs(rnt, curr, candidates, used, target, sum+candidates[idx], idx+1);
+        curr.remove(curr.size()-1);
+        used[idx] = false;
+        
+        
     }
-    
 }
