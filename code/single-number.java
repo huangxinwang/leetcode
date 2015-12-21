a4b1e0d7682d825a40fbb4db5cedd989110d13cd
Huangxin <maggiewangsjtu@gmail.com>
2015-12-20 21:27:47 -0500

    Update single-number.java

--- code/single-number.java
+++ code/single-number.java
@@ -8,51 +8,17 @@
  * a^b^c = a^(b^c)
  */
  
-public class Solution {
-    public int singleNumber(int[] A) {
-        
-        int rnt = 0;
-        for(int i=0; i<A.length; i++)
-        {
-            //xor
-            rnt = rnt^A[i];
-        }
-        
-        return rnt;
-    }
-}
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////
-//Round 2: 11/30/2014
-
-//time complexity O(n)
-public class Solution {
-    public int singleNumber(int[] A) {
-        
-        int rnt = 0;
-        for(int i=0; i<A.length; i++){
-            rnt = rnt^A[i];
-        }
-        
-        return rnt;
-    }
-}
-
-
-/////////////////////////////////////////
-// 2015/11/13
-// Highlight: xor
+////////////////////////////////////
+// 2015/12/20
+// Highlight: bit operation
 public class Solution {
     public int singleNumber(int[] nums) {
-        
-        int rnt = nums[0];
-        
-        //(a ^ b) ^ b = a
-        for(int i=1; i<nums.length; i++){
-            rnt = rnt ^ nums[i];
+        //0 xor x equals x
+        int rnt = 0;
+        //x xor x equals 0
+        for(int i=0; i<nums.length; i++){
+            rnt ^= nums[i];
         }
-        
         return rnt;
     }
 }
