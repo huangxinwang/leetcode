c219d238770031a53c65e7c39a504ae306dcbc39
Huangxin <maggiewangsjtu@gmail.com>
2015-12-20 23:11:13 -0500

    Update Median-of-Two-Sorted-Arrays.java

--- code/Median-of-Two-Sorted-Arrays.java
+++ code/Median-of-Two-Sorted-Arrays.java
@@ -4,52 +4,50 @@
  * 
  */
 
+/////////////////////////////////////////////
+// 2015/12/20
+// Highlight: divide and conquer
+// Time complexity: O(log(m+n))
 public class Solution {
-    public double findMedianSortedArrays(int A[], int B[]) {
-        
-        int m = A.length;
-        int n= B.length;
-        int sum = m+n;
-        if(sum%2==1)
-            return help(A, 0, m-1, B, 0, n-1, (m+n)/2);
-        else return (help(A, 0, m-1, B, 0, n-1, (m+n)/2)+help(A, 0, m-1, B, 0, n-1, (m+n)/2-1))/2;
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        int total = len1+len2;
+        if(total%2==1){
+            return help(nums1, 0, len1-1, nums2, 0, len2-1, total/2); //find the kth one
+        }else{
+            double val1 = help(nums1, 0, len1-1, nums2, 0, len2-1, total/2);
+            double val2 = help(nums1, 0, len1-1, nums2, 0, len2-1, total/2-1);
+            return (val1+val2)/2;
+        }
     }
     
-    //find the (k+1)-th element
-    public double help(int A[], int astart, int aend, int[] B, int bstart, int bend, int k){
+    //Find the k-th element (index is k)
+    public double help(int[] nums1, int start1, int end1, int[] nums2, int start2, int end2, int k){
+        int len1 = end1-start1+1;
+        int len2 = end2-start2+1;
+        if(len1==0) return nums2[start2+k];
+        if(len2==0) return nums1[start1+k];
+       
+        if(k==0) return Math.min(nums1[start1], nums2[start2]);
         
-        //check A and B's length
-        int alen = aend-astart+1;
-        int blen = bend-bstart+1;
+        //find mid of nums1, note that the mid is porportional to "k" and "len1/(len1+len2)"
+        int mid1 = len1*k/(len1+len2);
+        int mid2 = start2 + k-mid1-1;
+        mid1 = start1 + mid1;
         
-        //special case
-        if(alen==0)
-            return B[bstart+k];
-        if(blen==0)
-            return A[astart+k];
-        if(k==0)
-            return A[astart] < B[bstart]? A[astart]: B[bstart];
-          
-        //get mid
-        int amid = alen*k/(alen+blen);
-        int bmid = k - amid-1;
-        amid = astart+amid;
-        bmid = bstart+bmid;
-        
-        if(A[amid] > B[bmid]){
-            k = k-(bmid-bstart+1);
-            aend = amid;
-            bstart = bmid+1;
-        }
-        else{
-            k = k-(amid-astart+1);
-            bend = bmid;
-            astart = amid+1;
-        }
-        
-        return help(A, astart, aend, B, bstart, bend, k);
+        int val1 = nums1[mid1];
+        int val2 = nums2[mid2];
+        //Case 1: discard left part of nums1 (i.e., find the minimum x elements of nums)
+        if(val1<val2) return help(nums1, mid1+1, end1, nums2, start2, end2, k-(mid1-start1)-1);
+        //Case 2: discard left part of nums2
+        else if(val1>val2) return help(nums1, start1, end1, nums2, mid2+1, end2, k-(mid2-start2)-1);
+        //Case 3
+        else return val1;
     }
 }
+
+
 
 
 /////////////////////////////////////////////////////////
