1a7f5b7a36705b8e388cfe5d523c1f78fc3475ee
Huangxin <maggiewangsjtu@gmail.com>
2015-12-24 23:30:48 -0500

    Update Rotate-Image.java

--- code/Rotate-Image.java
+++ code/Rotate-Image.java
@@ -9,65 +9,23 @@
  */
 
 
+////////////////////////////////////////////////////////////////////
+// 2015/12/24
+// Highlight: in-place rotate
+
 public class Solution {
     public void rotate(int[][] matrix) {
-        
         int n = matrix.length;
         
-        //use the relation that matrix[i][j] = matrix[n-1-j][i]
-        for(int i=0; i<n/2; i++){
-            //note that it should not be j<n/2
-            for(int j=0; j < (n+1) /2; j++){
-                //in place rotate
+        //use the logic of matrix[i][j] = matrix[n-1-j][i]
+        for(int i=0; i<(n+1)/2; i++){
+            //rotate by each layer
+            for(int j=i; j<n-i-1; j++){
                 int tmp = matrix[i][j];
                 matrix[i][j] = matrix[n-1-j][i];
                 matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
                 matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
                 matrix[j][n-1-i] = tmp;
-            }
-        }
-            
-        
-    }
-}
-
-//////////////////////////////////////////////////////////////////
-//Round 2: 12/30/2014
-public class Solution {
-    public void rotate(int[][] matrix) {
-        
-        int n = matrix.length;
-        
-        //use the logic: matrix[i][j] = matrix[n-j-1][i]
-        for(int i=0; i<(n+1)/2; i++){
-            for(int j=0; j<n/2; j++){
-              
-                int tmp = matrix[i][j];
-                matrix[i][j] = matrix[n-j-1][i];
-                matrix[n-j-1][i] = matrix[n-i-1][n-j-1];
-                matrix[n-i-1][n-j-1] = matrix[j][n-i-1];
-                matrix[j][n-i-1] = tmp;
-            }
-        }
-    }
-}
-
-
-////////////////////////////////////////////////////////////////////
-// 2015/11/05
-// Highlight: in-place rotate
-public class Solution {
-    public void rotate(int[][] matrix) {
-        int n = matrix.length;
-        
-        //rotate by each layer
-        for(int i=0; i<n/2; i++){
-            for(int j=i; j<n-i-1; j++){
-                int tmp = matrix[i][j];
-                matrix[i][j] = matrix[n-j-1][i];
-                matrix[n-j-1][i] = matrix[n-i-1][n-j-1];
-                matrix[n-i-1][n-j-1] = matrix[j][n-i-1];
-                matrix[j][n-i-1] = tmp;
             }
         }
     }
