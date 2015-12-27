ddb465d59baa4e9b40943e80a6bd522f3d372c8c
Huangxin <maggiewangsjtu@gmail.com>
2015-12-26 21:46:26 -0500

    Update Two-Sum.java

--- code/Two-Sum.java
+++ code/Two-Sum.java
@@ -8,19 +8,25 @@
 // 2015/11/30
 // Highlight: hashmap, check if (target-curr) is in the map
 // Time: O(n)
+// Space: O(n)
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        
+        //put each number and its index in hashmap
+        //overwrite repeated numbers
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
         for(int i=0; i<nums.length; i++){
-            map.put(nums[i],i+1);
+            map.put(nums[i], i);
         }
         
+        //for each number, check whether (target-remain) is in the map 
+        //make sure two indexes are different
         for(int i=0; i<nums.length; i++){
             int remain = target - nums[i];
-            if(map.containsKey(remain) && map.get(remain)!=i+1){
+            if(map.containsKey(remain) && map.get(remain)!=i){
                 int[] rnt = new int[2];
                 rnt[0] = i+1;
-                rnt[1] = map.get(remain);
+                rnt[1] = map.get(remain)+1;
                 return rnt;
             }
         }
