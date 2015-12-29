d9b22050b8191e75b493b9486ce28bd5bf3117a8
Huangxin <maggiewangsjtu@gmail.com>
2015-12-28 21:11:58 -0500

    Update Best-Time-to-Buy-and-Sell-Stock.java

--- code/Best-Time-to-Buy-and-Sell-Stock.java
+++ code/Best-Time-to-Buy-and-Sell-Stock.java
@@ -6,74 +6,22 @@
  * design an algorithm to find the maximum profit.
  */
 
-
-public class Solution {
-    public int maxProfit(int[] prices) {
-
-        //special case
-        if(prices.length<=1) return 0;
-        
-        //init
-        int profit = 0;
-        int min = prices[0];
-        int max = prices[0];
-        
-        //traverse and record best profit
-        for(int i=1; i<prices.length; i++){
-            //update max
-            if(max<prices[i]){
-                 max = prices[i];
-                 int tmp = max - min;
-                 profit = profit>tmp ? profit : tmp;
-            }
-            //update min
-            else if(min>prices[i]){
-                min = prices[i];
-                max = prices[i];
-            }
-        }
-        
-        return profit;
-    }
-}
-
-//////////////////////////////////////////////////////////////////////////
-//Round 2: 12/04/2014
-//Time complexity O(n)
-public class Solution {
-    public int maxProfit(int[] prices) {
-        if(prices.length<=1) return 0;
-        int min = prices[0];
-        int maxpro = 0;
-        for(int i=1; i<prices.length; i++){
-            if(prices[i] - min > maxpro)
-                maxpro = prices[i] - min;
-            if(prices[i] < min)
-                min = prices[i];
-        }
-        
-        return maxpro;
-    }
-}
-
-
-//////////////////////////////////////////////
-// 2015/11/11
+/////////////////////////////////////
+// 2015/12/28
 // Highlight: keep track of min value
+
 public class Solution {
     public int maxProfit(int[] prices) {
-        int profit = 0;
-        if(prices.length<=1) return profit;
-        
+        if(prices==null || prices.length==0) return 0;
+        int rnt = 0;
         int min = prices[0];
         
-        //keep min and max value
         for(int i=1; i<prices.length; i++){
-            if(prices[i]-min >=profit)
-                profit = prices[i] - min;
-            if(prices[i] < min) min = prices[i];;
+            //update min value before or at the i-th days
+            min = Math.min(prices[i],min);
+            //calculate the profit
+            rnt = Math.max(rnt, prices[i]-min);
         }
-        
-        return profit;
+        return rnt;
     }
 }
