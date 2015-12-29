65e473fcc1dd023541dd02ecdf74940faeb37789
Huangxin <maggiewangsjtu@gmail.com>
2015-12-29 01:28:17 -0500

    Update Best-Time-to-Buy-and-Sell-Stock-II.java

--- code/Best-Time-to-Buy-and-Sell-Stock-II.java
+++ code/Best-Time-to-Buy-and-Sell-Stock-II.java
@@ -8,61 +8,17 @@
  * However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
  */
 
-
-
-public class Solution {
-    public int maxProfit(int[] prices) {
-        
-        int profit = 0;
-        
-        //trick
-        for(int i=0; i<prices.length-1; i++){
-            profit += Math.max(0,prices[i+1]-prices[i]);
-        }
-        
-        return profit;
-    }
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////
-//Round 2: 12/04/2014
-//time complexity O(n)
-
+//////////////////////////////////
+// 2015/12/29
+// Highlight: sum up all possible increase
 
 public class Solution {
     public int maxProfit(int[] prices) {
-        
-        if(prices.length <=1) return 0;
-        int maxpro = 0;
-
-        //sell the stock whenever it is possible to make profits
+        int rnt = 0;
+        //sum up all possible increase
         for(int i=1; i<prices.length; i++){
-            if(prices[i] > prices[i-1])
-                maxpro += prices[i] - prices[i-1];
-            else continue;
+            rnt += Math.max(0, prices[i]-prices[i-1]);
         }
-        
-        return maxpro;
-        
-    }
-}
-
-/////////////////////////////////////////////////////////
-// 2015/11/12
-// Highlight: keep track of current min value
-public class Solution {
-    public int maxProfit(int[] prices) {
-        if(prices.length<=1) return 0;
-        int profit = 0;
-        int currmin = prices[0];
-        
-        for(int i=1; i<prices.length; i++){
-            if(prices[i]>currmin){
-                profit += (prices[i]-currmin);
-            }
-            currmin = prices[i];
-        }
-        
-        return profit;
+        return rnt;
     }
 }
